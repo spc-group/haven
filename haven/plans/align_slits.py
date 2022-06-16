d909272b82ec21718e4abeef1782a75e528032bb
Mark Wolfman <canismarko@gmail.com>
2022-06-15 21:21:48 -0500

    Added logging and poor-fit warnings to align_slits plan.

--- haven/plans/align_slits.py
+++ haven/plans/align_slits.py
@@ -3,6 +3,9 @@ to that position.
 
 """
 
+import logging
+import warnings
+
 import numpy as np
 import lmfit
 from bluesky import plans as bp, plan_stubs as bps
@@ -13,12 +16,18 @@ from bluesky.callbacks import LiveFit
 __all__ = ["align_slits"]
 
 
+log = logging.getLogger(__name__)
+
+
 def align_slit_scan(slit_motor, ion_chamber):
     yield from bp.rel_scan([ion_chamber], slit_motor, -1, 1, 50)
 
 
 def align_slits(slit_motors=[], ion_chamber=None):
-    """Scan a range of values for the slit motor, and position the motor in the center."""
+    """Scan a range of values for the slit motor, and position the
+    motor in the center.
+
+    """
     for motor in slit_motors:
         # Set up the live fit object to process the results
         def gaussian(x, A, sigma, x0):
@@ -28,8 +37,17 @@ def align_slits(slit_motors=[], ion_chamber=None):
         init_guess = {"A": 2, "sigma": lmfit.Parameter("sigma", 3, min=0), "x0": -0.2}
         fit = LiveFit(model, ion_chamber.name, {"x": motor.name}, init_guess)
         scan = subs_decorator(fit)(align_slit_scan)
-        # Execute the scan
+        # Execute the scan to find the center
         yield from scan(ion_chamber=ion_chamber, slit_motor=motor)
         # Move the slit motor to the center of the Gaussian peak
         new_center = fit.result.values["x0"]
         yield from bps.mv(motor, new_center)
+        fit_quality = fit.result.chisqr
+        # Logging, severity determined by quality of fit
+        log.info(f"Centered slit '{motor.name}': {new_center} (X²: {fit_quality:.2f})")
+        if fit_quality > 1:
+            msg = (
+                f"Poor fit while centering motor '{motor.name}'. X²: {fit_quality:.2f}"
+            )
+            log.warning(msg)
+            warnings.warn(msg, RuntimeWarning)
--- tests/test_plans.py
+++ tests/test_plans.py
@@ -1,9 +1,14 @@
 import unittest
 import time
+import warnings
+import logging
 
 from run_engine import RunEngineStub
 from haven import align_slits
 from ophyd import sim
+
+
+logging.basicConfig(level=logging.INFO)
 
 
 def my_callback(name, doc):
@@ -38,3 +43,27 @@ class PlanUnitTests(unittest.TestCase):
         self.RE(align_slits(slit_motors=[slit_motor], ion_chamber=I0))
         # Check that the slit positions have been set
         self.assertEqual(slit_motor.position, -0.5)
+
+    def test_warn_poor_fit(self):
+        """Check that the plan emits a warning when no good fit is detected."""
+        # Prepare a fake detector and slit motor
+        slit_motor = sim.motor
+        I0 = sim.SynGauss(
+            "det",
+            sim.motor,
+            "motor",
+            center=-0.5,
+            noise="poisson",
+            noise_multiplier=100,
+            Imax=1,
+            sigma=1,
+            labels={"detectors"},
+        )
+        # Execute the plan, catching warnings
+        with warnings.catch_warnings(record=True) as w:
+            # Cause all warnings to always be triggered.
+            warnings.simplefilter("always")
+            self.RE(align_slits(slit_motors=[slit_motor], ion_chamber=I0))
+            # Check that a warning was raised
+            self.assertEqual(len(w), 1)
+            self.assertIn("Poor fit while centering motor", str(w[0].message))
