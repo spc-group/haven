2b89c719c3918326ff2eb229c2d5a271fe9daf99
Sector 25 ID-C User <s25idcuser@microprobe.xray.aps.anl.gov>
2022-11-15 16:10:09 -0600

    Added basic ability to retrieve data from the databroker catalog.

--- haven/__init__.py
+++ haven/__init__.py
@@ -2,6 +2,7 @@ __all__ = ["energy_scan"]
 
 #  Top-level imports
 from . import positioners  # noqa: F401
+from .catalog import load_catalog, load_result, load_data  # noqa: F401
 from .plans.align_slits import align_slits  # noqa: F401
 from .energy_ranges import ERange, KRange, merge_ranges  # noqa: F401
 from .plans.energy_scan import energy_scan  # noqa: F401
---
+++ haven/catalog.py
@@ -0,0 +1,77 @@
+import databroker
+
+def load_catalog(name: str="bluesky"):
+    """Load a databroker catalog for retrieving data.
+
+    To retrieve individual scans, consider the ``load_result`` and
+    ``load_data`` methods.
+
+    Parameters
+    ==========
+    name
+      The name of the catalog as defined in the Intake file
+      (e.g. ~/.local/share/intake/catalogs.yml)
+    
+    Returns
+    =======
+    catalog
+      The databroker catalog.
+    """
+    return databroker.catalog[name]
+    
+
+def load_result(uid: str, catalog_name: str="bluesky", stream: str="primary"):
+    """Load a past experiment from the database.
+
+    The result contains metadata and scan parameters. The data
+    themselves are accessible from the result's *read()* method.
+
+    Parameters
+    ==========
+    uid
+      The universal identifier for this scan, as return by a bluesky
+      RunEngine.
+    catalog_name
+      The name of the catalog as defined in the Intake file
+      (e.g. ~/.local/share/intake/catalogs.yml)      
+    stream
+      The data stream defined by the bluesky RunEngine.
+    
+    Returns
+    =======
+    result
+      The experiment result, with data available via the *read()*
+      method.
+
+    """
+    cat = load_catalog(name=catalog_name)
+    result = cat[uid][stream]
+    return result
+
+
+def load_data(uid, catalog_name="bluesky", stream="primary"):
+    """Load a past experiment's data from the database.
+
+    The result is an xarray with the data collected.
+
+    Parameters
+    ==========
+    uid
+      The universal identifier for this scan, as return by a bluesky
+      RunEngine.
+    catalog_name
+      The name of the catalog as defined in the Intake file
+      (e.g. ~/.local/share/intake/catalogs.yml)      
+    stream
+      The data stream defined by the bluesky RunEngine.
+    
+    Returns
+    =======
+    data
+      The experimental data, as an xarray.
+
+    """
+
+    res = load_result(uid=uid, catalog_name=catalog_name, stream=stream)
+    data = res.read()
+    return data
