55cdc7e09efe90ddb7d1263e3f1d322a58452fba
Sector 25 ID-C User <s25idcuser@microprobe.xray.aps.anl.gov>
2022-12-10 14:51:53 -0600

    Updated PVs for new mono IOC.

--- haven/instrument/monochromator.py
+++ haven/instrument/monochromator.py
@@ -14,23 +14,9 @@ class Monochromator(Device):
     roll_int = Cpt(EpicsMotor, ":m7", labels={"motors"}, kind="config")
     pi_int = Cpt(EpicsMotor, ":m8", labels={"motors"}, kind="config")
     mode = Cpt(EpicsSignal, ":mode", labels={"motors"}, kind="config")
-    energy = FCpt(EpicsMotor, "{energy_prefix}:Energy", labels={"motors"})
-
-    def __init__(
-        self, prefix, *args, name, energy_prefix=None, id_prefix=None, **kwargs
-    ):
-        # Use the default ioc for energy if not explicitly set
-        if energy_prefix is None:
-            energy_prefix = prefix
-        self.energy_prefix = energy_prefix
-        self.id_prefix = id_prefix
-        super().__init__(prefix, name=name, *args, **kwargs)
+    energy = Cpt(EpicsMotor, ":Energy", labels={"motors"}, kind="hinted")
 
 
 def load_monochromator(config):
-    monochromator = Monochromator(
-        config["monochromator"]["ioc"],
-        energy_prefix=config["monochromator"]["energy_ioc"],
-        name="monochromator",
-    )
+    monochromator = Monochromator(config["monochromator"]["ioc"], name="monochromator")
     return monochromator
