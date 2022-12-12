98d371d3c3d384819ce567ce2f04e4d4daba2cb2
Mark Wolfman <canismarko@gmail.com>
2022-12-11 22:14:10 -0600

    Changed monochromator motor PVs based on Max's new IOC.

--- haven/instrument/monochromator.py
+++ haven/instrument/monochromator.py
@@ -5,16 +5,19 @@ from .instrument_registry import registry
 
 @registry.register
 class Monochromator(Device):
-    horiz = Cpt(EpicsMotor, ":m1", labels={"motors"}, kind="config")
-    vert = Cpt(EpicsMotor, ":m2", labels={"motors"}, kind="config")
-    bragg = Cpt(EpicsMotor, ":m3", labels={"motors"})
-    gap = Cpt(EpicsMotor, ":m4", labels={"motors"})
-    roll2 = Cpt(EpicsMotor, ":m5", labels={"motors"}, kind="config")
-    pitch2 = Cpt(EpicsMotor, ":m6", labels={"motors"}, kind="config")
-    roll_int = Cpt(EpicsMotor, ":m7", labels={"motors"}, kind="config")
-    pi_int = Cpt(EpicsMotor, ":m8", labels={"motors"}, kind="config")
+    # Virtual positioners
     mode = Cpt(EpicsSignal, ":mode", labels={"motors"}, kind="config")
     energy = Cpt(EpicsMotor, ":Energy", labels={"motors"}, kind="hinted")
+    offset = Cpt(EpicsMotor, ":Offset", labels={"motors"}, kind="config")
+    # ACS Motors
+    horiz = Cpt(EpicsMotor, ":ACS:m1", labels={"motors"}, kind="config")
+    vert = Cpt(EpicsMotor, ":ACS:m2", labels={"motors"}, kind="config")
+    bragg = Cpt(EpicsMotor, ":ACS:m3", labels={"motors"})
+    gap = Cpt(EpicsMotor, ":ACS:m4", labels={"motors"})
+    roll2 = Cpt(EpicsMotor, ":ACS:m5", labels={"motors"}, kind="config")
+    pitch2 = Cpt(EpicsMotor, ":ACS:m6", labels={"motors"}, kind="config")
+    roll_int = Cpt(EpicsMotor, ":ACS:m7", labels={"motors"}, kind="config")
+    pi_int = Cpt(EpicsMotor, ":ACS:m8", labels={"motors"}, kind="config")
 
 
 def load_monochromator(config):
