4918d1f791dd76df09a00779c960dcd5ff9820d7
Mark Wolfman <canismarko@gmail.com>
2022-12-20 12:04:59 -0600

    Updated docstrings for some instrument devices.

--- haven/instrument/energy_positioner.py
+++ haven/instrument/energy_positioner.py
@@ -7,6 +7,7 @@ from ophyd import (
     PVPositioner,
     EpicsSignal,
     EpicsSignalRO,
+    OphydObject
 )
 from ophyd.pseudopos import pseudo_position_argument, real_position_argument
 
@@ -16,6 +17,9 @@ from .instrument_registry import registry
 from .monochromator import Monochromator
 
 
+__all__ = ["EnergyPositioner", "load_energy_positioner"]
+
+
 class Undulator(PVPositioner):
     setpoint = Cpt(EpicsSignal, ":ScanEnergy.VAL")
     readback = Cpt(EpicsSignalRO, ":Energy.VAL")
@@ -23,18 +27,54 @@ class Undulator(PVPositioner):
     stop_signal = Cpt(EpicsSignal, ":Stop.VAL", kind="omitted")
 
 
-# @registry.register
 class EnergyPositioner(PseudoPositioner):
-    id_offset = 155  # In eV
+    """The operational energy of the beamline.
+
+    Responsible for setting both mono and ID energy with an optional
+    ID offset. Setting the *energy* component will propagate to both
+    real devices and so ``EnergyPositioner().energy`` is a good
+    candidate for a positioner for Bluesky plans.
+
+    Currently, the offset between the insertion device and the
+    monochromator is fixed. In the future this will be replaced with a
+    more sophisticated calculation.
+
+    .. todo::
+
+       Insert functionality to have a non-constant ID offset.
+
+    Attributes
+    ==========
+
+    id_offset
+      The offset for the insertion device relative to the mono energy.
+    energy
+      The pseudo positioner for the forward calculation.
+    mono_energy
+      The real component for the monochromator energy.
+    id_energy
+      The real component for the insertion device energy.
+
+    Parameters
+    ==========
+    mono_pv
+      The process variable for the monochromator energy PV.
+    id_prefix
+      The prefix for the insertion device energy, such that
+      f"{id_prefix}:Energy.VAL" reaches the energy readback value.
+
+    """
+    id_offset: float = 155.  # In eV
 
     # Pseudo axes
-    energy = Cpt(PseudoSingle)
+    energy: OphydObject = Cpt(PseudoSingle)
 
     # Equivalent real axes
-    mono_energy = FCpt(EpicsMotor, "{mono_pv}")
-    id_energy = FCpt(Undulator, "{id_prefix}")
+    mono_energy: OphydObject = FCpt(EpicsMotor, "{mono_pv}")
+    id_energy: OphydObject = FCpt(Undulator, "{id_prefix}")
 
-    def __init__(self, mono_pv, id_prefix, *args, **kwargs):
+    def __init__(self, mono_pv: str, id_prefix: str, *args, **kwargs):
+        """INIT DOCSTRING"""
         self.mono_pv = mono_pv
         self.id_prefix = id_prefix
         super().__init__(*args, **kwargs)
