28579c61610f9c44d2b00601d3a439a8f7b2ce51
Mark Wolfman <canismarko@gmail.com>
2023-02-09 22:19:17 -0600

    Ability to enable or disable ROIs.

--- haven/instrument/fluorescence_detector.py
+++ haven/instrument/fluorescence_detector.py
@@ -1,12 +1,15 @@
+from enum import IntEnum
+from collections import OrderedDict
+from typing import Optional, Sequence
+
 from ophyd import (
     mca,
     Device,
     EpicsSignal,
     Component as Cpt,
+    DynamicDeviceComponent as DDC,
     Kind,
 )
-from enum import IntEnum
-
 
 from .scaler_triggered import ScalerTriggered
 from .instrument_registry import registry
@@ -14,20 +17,176 @@ from .._iconfig import load_config
 from .. import exceptions
 
 
+default_kind = Kind.normal | Kind.config
+
+
+class ROI(mca.ROI):
+    _default_read_attrs = [
+        "count",
+        "net_count",
+    ]
+    _default_configuration_attrs = [
+        "label",
+        "bkgnd_chans",
+        "hi_chan",
+        "lo_chan",
+    ]
+    kind = default_kind
+
+
+def add_rois(range_, kind=Kind.omitted, **kwargs):
+    """Add one or more ROIs to an MCA instance
+    
+    Parameters
+    ----------
+    range_ : sequence of ints
+        Must be be in the set [0,31]
+
+    By default, an EpicsMCA is initialized with all 32 rois. These
+    provide the following Components as EpicsSignals (N=[0,31]):
+    EpicsMCA.rois.roiN.(label,count,net_count,preset_cnt, is_preset,
+    bkgnd_chans, hi_chan, lo_chan)
+
+    """
+    defn = OrderedDict()
+    # import pdb; pdb.set_trace()
+    kwargs["kind"] = kind
+    for roi in range_:
+        if not (0 <= roi < 32):
+            raise ValueError("roi must be in the set [0,31]")
+        attr = "roi{}".format(roi)
+        defn[attr] = (ROI, ".R{}".format(roi), kwargs,)
+    return defn
+
+
+class MCARecord(mca.EpicsMCARecord):
+    rois = DDC(add_rois(range(0, 32)), kind=default_kind)
+    _default_read_attrs = ["rois"]
+
+
 @registry.register
 class DxpDetectorBase(mca.EpicsDXPMultiElementSystem):
     """A fluorescence detector based on XIA-DXP XMAP electronics.
 
-    By itself, this class has no multi-channel-analyzers, and so will
-    not really be very useful. It should have
-    ``ophyd.mca.EpicsMCARecord`` objects added as components.
+    Creates MCA components based on the number of elements.
     
     """
+    _default_read_attrs = [
+        'preset_live_time',
+        'preset_real_time',
+        'dead_time',
+        'elapsed_live',
+        'elapsed_real',
+        'mca1',
+        'mca2',
+        'mca3',
+        'mca4',
+    ]
+    _default_configuration_attrs = [
+        'max_scas',
+        'num_scas',
+        'poll_time',
+        'prescale',
+        'preset_mode',
+        'preset_events',
+        'preset_triggers',
+        'snl_connected',
+    ]
+    _omitted_attrs = [  # Just save them here for easy reference
+        'channel_advance',
+        'client_wait',
+        'dwell',
+        'save_system',
+        'save_system_file',
+        'set_client_wait',
+        'erase_all',
+        'erase_start',
+        'start_all',
+        'stop_all',
+        'set_acquire_busy',
+        'acquire_busy',
+        'status_all',
+        'status_all_once',
+        'acquiring',
+        'read_baseline_histograms',
+        'read_all',
+        'read_all_once',
+        'copy_adcp_ercent_rule',
+        'copy_baseline_cut_enable',
+        'copy_baseline_cut_percent',
+        'copy_baseline_filter_length',
+        'copy_baseline_threshold',
+        'copy_decay_time',
+        'copy_detector_polarity',
+        'copy_energy_threshold',
+        'copy_gap_time',
+        'copy_max_energy',
+        'copy_max_width',
+        'copy_peaking_time',
+        'copy_preamp_gain',
+        'copy_roic_hannel',
+        'copy_roie_nergy',
+        'copy_roi_sca',
+        'copy_reset_delay',
+        'copy_trigger_gap_time',
+        'copy_trigger_peaking_time',
+        'copy_trigger_threshold',
+        'do_read_all',
+        'do_read_baseline_histograms',
+        'do_read_traces',
+        'do_status_all',
+        'read_low_level_params',
+        'read_traces',
+        'trace_modes',
+        'trace_times',
+        'idead_time',
+    ]
+    
+    def mcas(self):
+        mcas = [getattr(self, m) for m in self.component_names if m.startswith("mca")]
+        return mcas
 
+    def rois(self, roi_indices: Optional[Sequence[int]] = None):
+        # Get the list of ROIs to activate
+        all_rois = []
+        for mca in self.mcas():
+            rois = mca.rois.component_names
+            rois = [getattr(mca.rois, r) for r in rois]
+            # Get sub-list if requested
+            if roi_indices is not None:
+                rois = [rois[i] for i in roi_indices]
+            all_rois.extend(rois)
+        return all_rois
+    
+    def enable_rois(self, rois: Optional[Sequence[int]] = None):
+        """Include some, or all, ROIs in the list of detectors to
+        read.
+
+        rois
+          A list of indices for which ROIs to enable. Default is to
+          operate on all ROIs.
+
+        """
+        for roi in self.rois(roi_indices=rois):
+            roi.kind = default_kind
+
+
+    def disable_rois(self, rois: Optional[Sequence[int]] = None):
+        """Remove some, or all, ROIs from the list of detectors to
+        read.
+
+        rois
+          A list of indices for which ROIs to enable. Default is to
+          operate on all ROIs.
+
+        """
+        for roi in self.rois(roi_indices=rois):
+            roi.kind = Kind.omitted
+               
 
 class XspressDetector(ScalerTriggered, Device):
     """A fluorescence detector plugged into an Xspress3 readout."""
-
+    
     num_frames = Cpt(EpicsSignal, "NumImages", kind=Kind.config)
     trigger_mode = Cpt(EpicsSignal, "TriggerMode", kind=Kind.config)
     acquire = Cpt(EpicsSignal, "Acquire", kind=Kind.omitted)
@@ -69,8 +228,9 @@ class XspressDetector(ScalerTriggered, Device):
 def load_dxp_detector(device_name, prefix, num_elements):
     # Build the mca components
     mca_names = [f"mca{n}" for n in range(1, num_elements+1)]
-    mcas = {mname: Cpt(mca.EpicsMCARecord, suffix=mname, name=mname)
+    mcas = {mname: Cpt(MCARecord, suffix=mname, name=mname)
             for mname in mca_names}
+    # Add the ROIs to the list of readable detectors
     # Create a dynamic subclass
     class_name = device_name.title().replace("_", "")
     parent_classes = (DxpDetectorBase,)
@@ -92,4 +252,3 @@ def load_fluorescence_detectors(config=None):
         else:
             msg = f"Electronics '{cfg['electronics']}' for {name} not supported."
             raise exceptions.UnknownDeviceConfiguration(msg)
-        
--- tests/test_fluorescence.py
+++ tests/test_fluorescence.py
@@ -1,7 +1,10 @@
+from unittest.mock import MagicMock
+from ophyd import Kind
+from bluesky import plans as bp
+
 import pytest
 
 from epics import caget
-from haven.instrument.fluorescence_detector import XspressDetector, load_fluorescence_detectors
 
 
 @pytest.fixture
@@ -26,12 +29,100 @@ def test_staging(vortex, ioc_vortex):
     assert caget("xspress:TriggerMode") == 1
 
 
-def test_load_dxp(sim_registry):
+def _ensure_connected(*args, **kwargs):
+    pass
+
+
+def test_load_dxp(sim_registry, mocker):
+    mocker.patch('ophyd.signal.EpicsSignalBase._ensure_connected')
+    from haven.instrument.fluorescence_detector import load_fluorescence_detectors
     load_fluorescence_detectors(config=None)
     # See if the device was loaded
-    sim_registry.find(name="vortex_me4")
-    
+    vortex = sim_registry.find(name="vortex_me4")
+    # Patch some 
+    # Check that the MCA's are available
+    assert hasattr(vortex, "mca1")
+    assert hasattr(vortex, "mca2")
+    assert hasattr(vortex, "mca3")
+    assert hasattr(vortex, "mca4")
+    # Check that MCA's have ROI's available
+    assert hasattr(vortex.mca1, "rois")
+    assert hasattr(vortex.mca1.rois, "roi1")
 
+
+@pytest.fixture()
+def vortex(sim_registry, mocker):
+    mocker.patch('ophyd.signal.EpicsSignalBase._ensure_connected')
+    from haven.instrument.fluorescence_detector import load_fluorescence_detectors
+    load_fluorescence_detectors(config=None)
+    # See if the device was loaded
+    vortex = sim_registry.find(name="vortex_me4")
+    yield vortex
+
+
+def test_enable_rois(vortex):
+    assert hasattr(vortex.mca1, "rois")
+    # Test that all channels are disabled by default
+    assert "vortex_me4_mca1_rois_roi1" not in vortex.read_attrs
+    assert "preset_live_time" in vortex.read_attrs
+    assert "preset_mode" in vortex.configuration_attrs
+    # Make sure config attrs are in the right place
+    config_signals = [
+        'mca3.rois.roi0.label',
+        'mca3.rois.roi0.bkgnd_chans',
+        'mca3.rois.roi0.hi_chan',
+        'mca3.rois.roi0.lo_chan',
+    ]
+    normal_signals = ['mca3.rois.roi0.count', 'mca3.rois.roi0.net_count']
+    omitted_signals = ['mca3.rois.roi0.preset_count',
+                     'mca3.rois.roi0.is_preset']
+    for attr in config_signals:
+        assert attr not in vortex.configuration_attrs
+        assert attr not in vortex.read_attrs
+    for attr in normal_signals:
+        assert attr not in vortex.read_attrs
+        assert attr not in vortex.configuration_attrs
+    for attr in omitted_signals:
+        assert attr not in vortex.read_attrs
+        assert attr not in vortex.configuration_attrs
+    # Enable all ROIs
+    vortex.enable_rois()
+    for attr in config_signals:
+        assert attr in vortex.configuration_attrs
+        assert attr not in vortex.read_attrs
+    for attr in normal_signals:
+        assert attr in vortex.read_attrs
+        assert attr not in vortex.configuration_attrs
+    for attr in omitted_signals:
+        assert attr not in vortex.read_attrs
+        assert attr not in vortex.configuration_attrs
+    # Disable all ROIs
+    vortex.disable_rois()
+    for attr in config_signals:
+        assert attr not in vortex.configuration_attrs
+        assert attr not in vortex.read_attrs
+    for attr in normal_signals:
+        assert attr not in vortex.read_attrs
+        assert attr not in vortex.configuration_attrs
+    for attr in omitted_signals:
+        assert attr not in vortex.read_attrs
+        assert attr not in vortex.configuration_attrs
+
+
+def test_enable_some_rois(vortex):
+    vortex.disable_rois()
+    assert "mca1.rois.roi0" not in vortex.read_attrs
+    assert "mca1.rois.roi1" not in vortex.read_attrs
+    # Enable just ROI0
+    vortex.enable_rois(rois=[0])
+    assert "mca1.rois.roi0" in vortex.read_attrs
+    assert "mca1.rois.roi1" not in vortex.read_attrs
+    # Disable just ROI0
+    vortex.enable_rois()
+    vortex.disable_rois(rois=[0])
+    assert "mca1.rois.roi0" not in vortex.read_attrs
+    assert "mca1.rois.roi1" in vortex.read_attrs    
+    
 
 @pytest.mark.xfail
 def test_with_plan(vortex):
