8fb71b3265dae68bcf50faa7c2c35db6a06c3082
Mark Wolfman <canismarko@gmail.com>
2023-02-21 22:09:45 -0600

    Added default read_attrs and config_attrs to the APS device.

--- haven/instrument/aps.py
+++ haven/instrument/aps.py
@@ -1,7 +1,27 @@
 from apstools.devices.aps_machine import ApsMachineParametersDevice
 from haven import registry
 
+
+class ApsMachine(ApsMachineParametersDevice):
+    _default_read_attrs = [
+        "current",
+        "lifetime",
+    ]
+    _default_configuration_attrs = [
+        "aps_cycle",
+        "machine_status",
+        "operating_mode",
+        "shutter_permit",
+        "fill_number",
+        "orbit_correction",
+        "global_feedback",
+        "global_feedback_h",
+        "global_feedback_v",
+        "operator_messages",
+    ]
+
+
 def load_aps(config=None):
     """Load devices related to the synchrotron as a whole."""
-    aps_ = ApsMachineParametersDevice(name="APS", labels={"synchrotrons"})
+    aps_ = ApsMachine(name="APS", labels={"synchrotrons"})
     registry.register(aps_)
--- tests/test_aps.py
+++ tests/test_aps.py
@@ -1,9 +1,26 @@
 import pytest
 
-from haven.instrument.aps import load_aps
+from haven.instrument.aps import load_aps, ApsMachine
 
 
 def test_load_aps(sim_registry):
     load_aps()
     aps = sim_registry.find(name="APS")
     assert hasattr(aps, "current")
+
+
+def test_read_attrs():
+    device = ApsMachine(name="Aps")
+    read_attrs = ["current", "lifetime"]
+    for attr in read_attrs:
+        assert attr in device.read_attrs
+
+
+def test_config_attrs():
+    device = ApsMachine(name="Aps")
+    config_attrs = ["aps_cycle", "machine_status", "operating_mode",
+                    "shutter_permit", "fill_number", "orbit_correction",
+                    "global_feedback", "global_feedback_h",
+                    "global_feedback_v", "operator_messages"]
+    for attr in config_attrs:
+        assert attr in device.configuration_attrs
