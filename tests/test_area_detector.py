e77c67f57354ace746c53c24c58c238d974c1f02
Mark Wolfman <canismarko@gmail.com>
2023-03-13 22:54:01 -0500

    Commented out area detector in default configuration.

--- tests/test_area_detector.py
+++ tests/test_area_detector.py
@@ -3,6 +3,13 @@ from ophyd.device import do_not_wait_for_lazy_connection
 from haven.instrument.area_detector import load_area_detectors
 
 def test_load_area_detectors(sim_registry, ioc_area_detector):
-    load_area_detectors()
+    load_area_detectors({
+        "area_detector": {
+            "sim_det": {
+                "prefix": "99idSimDet",
+                "device_class": "SimDetector",
+            }
+        }
+    })
     # Check that some area detectors were loaded
     dets = sim_registry.findall(label="area_detectors")
