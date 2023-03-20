4904a6968a55c0d55035c0f4dcc12ec83dfc4892
Sector 25 ID-C User <s25idcuser@microprobe.xray.aps.anl.gov>
2023-03-20 14:58:26 -0500

    Remove RunEngine class in favor of run_engine() factory.

--- haven/run_engine.py
+++ haven/run_engine.py
@@ -1,30 +1,48 @@
 import logging
 import warnings
 
-from bluesky import RunEngine as BlueskyRunEngine
+from bluesky import RunEngine as BlueskyRunEngine, suspenders
 import databroker
 
 from ._iconfig import load_config
 from .preprocessors import inject_haven_md_wrapper
+from .instrument.instrument_registry import registry
 
 log = logging.getLogger(__name__)
 
 
-class RunEngine(BlueskyRunEngine):
-    def __init__(self, *args, connect_databroker=True, **kwargs):
-        super().__init__(*args, **kwargs)
-        if connect_databroker:
-            # Load the databroker catalog and set up data saving
-            catalog_name = load_config()["database"]["databroker"]["catalog"]
-            try:
-                catalog = databroker.catalog[catalog_name]
-                self.subscribe(catalog.v1.insert)
-            except Exception as e:
-                msg = (
-                    f"Data are not being saved! Could not load databroker catalog: {e}"
-                )
-                log.error(msg)
-                warnings.warn(msg)
-                raise RuntimeError(msg)
-        # Add metadata pre-processor
-        self.preprocessors.append(inject_haven_md_wrapper)
+# class RunEngine(BlueskyRunEngine):
+#     def __init__(self, *args, connect_databroker=True, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         if connect_databroker:
+#             # Load the databroker catalog and set up data saving
+#             catalog_name = load_config()["database"]["databroker"]["catalog"]
+#             try:
+#                 catalog = databroker.catalog[catalog_name]
+#                 self.subscribe(catalog.v1.insert)
+#             except Exception as e:
+#                 msg = (
+#                     f"Data are not being saved! Could not load databroker catalog: {e}"
+#                 )
+#                 log.error(msg)
+#                 warnings.warn(msg)
+#                 raise RuntimeError(msg)
+#         # Add metadata pre-processor
+#         self.preprocessors.append(inject_haven_md_wrapper)
+
+
+def run_engine() -> BlueskyRunEngine:
+    RE = BlueskyRunEngine()
+    # Install suspenders
+    aps = registry.find("APS")
+    # Suspend when shutter permit is disabled
+    RE.install_suspender(
+        suspenders.SuspendWhenChanged(
+            signal=aps.shutter_permit,
+            expected_value="PERMIT",
+            allow_resume=True,
+            sleep=3,
+            tripped_message="Shutter permit revoked.",
+        )
+    )
+    return RE
--- ipython_startup.py
+++ ipython_startup.py
@@ -13,11 +13,7 @@ config = haven.load_config()
 print(f"Initializing {config['beamline']['name']}...", end="", flush=True)
 haven.load_instrument()
 print("done")
-RE = RunEngine()
-
-# Give the RunEngine time to initialize it's async magic?? (MFW)
-import time
-time.sleep(1.)
+RE = haven.run_engine()
 
 # Add metadata and data-saving to the run engine
 catalog = databroker.catalog['bluesky']
@@ -25,21 +21,3 @@ def save_data(name, doc):
     catalog.v1.insert(name, doc)
 RE.subscribe(save_data)
 RE.preprocessors.append(haven.preprocessors.inject_haven_md_wrapper)
-
-# Install suspenders
-aps = haven.registry.find("APS")
-
-# Suspend when shutter permit is disabled
-RE.install_suspender(
-    suspenders.SuspendWhenChanged(
-        signal=aps.shutter_permit,
-        expected_value="PERMIT",
-        allow_resume=True,
-        sleep=3,
-        tripped_message="Shutter permit revoked.",
-    )
-)
-
-# Monitor shutter open/close status for A and C stations
-for shutter in haven.registry.findall("shutters"):
-    RE.install_suspender(suspenders.SuspendBoolLow(shutter.pss_state, sleep=3))
