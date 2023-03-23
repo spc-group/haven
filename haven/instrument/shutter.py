a0d689cd74465f4d54d48beccdf17ccc9378c739
Mark Wolfman <canismarko@gmail.com>
2023-03-23 14:32:11 -0500

    Cleaned up shutter PVs for initalization.

--- haven/instrument/shutter.py
+++ haven/instrument/shutter.py
@@ -10,15 +10,21 @@ class Shutter(ApsPssShutterWithStatus):
     open_signal = FCpt(EpicsSignal, "{self.open_pv}")
     close_signal = FCpt(EpicsSignal, "{self.close_pv}")
 
-    def __init__(self, open_pv, close_pv, state_pv, *args, **kwargs):
+    def __init__(self, close_pv, state_pv, *args, open_pv=None, **kwargs):
+        """Either *open_pv* or *prefix* is a required keyword argument."""
+        # Check for a valid open PV
+        if open_pv is None:
+            try:
+                open_pv = kwargs.pop("prefix")
+            except KeyError:
+                raise TypeError(
+                    "Shutter() requires either *open_pv* or *prefix* arguments"
+                )
+        # Save PVs and instantiate the parent class
         self.open_pv = open_pv
         self.close_pv = close_pv
         self.state_pv = state_pv
         super().__init__(prefix=self.open_pv, state_pv=self.state_pv, *args, **kwargs)
-
-
-# A list of suspenders that will get populated once the shutters get loaded
-shutter_suspenders = []
 
 
 def load_shutters(config=None):
