474c3dee42783921be6700d5c5c9db83a7028b66
Sector 25 ID-C User <s25idcuser@microprobe.xray.aps.anl.gov>
2023-03-27 12:18:19 -0500

    Cleaned up the camera viewer UI.

--- firefly/camera_viewer.py
+++ firefly/camera_viewer.py
@@ -4,15 +4,25 @@ import subprocess
 import pyqtgraph
 import pydm
 import numpy as np
+import matplotlib.pyplot as plt
 
 import haven
 from firefly import display
+
+import sys
+np.set_printoptions(threshold=sys.maxsize)
+
 
 
 log = logging.getLogger(__name__)
 
 
+pyqtgraph.setConfigOption('imageAxisOrder', 'row-major')
+
+
 class CameraViewerDisplay(display.FireflyDisplay):
+    image_is_new: bool = True
+    
     def customize_device(self):
         addr = f"pva://{self.macros()['PREFIX']}Pva1:Image"
         self.image_channel = pydm.PyDMChannel(address=addr, value_slot=self.update_image)
@@ -23,13 +33,22 @@ class CameraViewerDisplay(display.FireflyDisplay):
         # Create the pyqtgraph image viewer
         self.image_view = pyqtgraph.ImageView(parent=self)
         self.ui.left_column_layout.addWidget(self.image_view)
+        # Connect signals for showing/hiding controls
+        self.ui.settings_button.clicked.connect(self.toggle_controls)
+
+    def toggle_controls(self):
+        # Show or hide the controls frame
+        controls_frame = self.ui.controls_frame
+        controls_frame.setVisible(not controls_frame.isVisible())
 
     def update_image(self, img):
-        # Put the RGB axis at the end
-        if img.ndim == 3:
-            img = np.moveaxis(img, 0, -1)
+        # For some reason the image comes out mishapen
+        new_shape = img.shape[::-1]
+        img = np.reshape(img, new_shape)
         # Show the image data
-        self.image_view.setImage(img)
+        self.image_view.setImage(img, autoRange=self.image_is_new, autoLevels=self.image_is_new)
+        # Update the display to indicate that we don't need to update levels/scale in the future
+        self.image_is_new = False
 
     def ui_filename(self):
         return "camera_viewer.ui"
