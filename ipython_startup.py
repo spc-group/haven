4b4d908ae7ca1624989716cf64925a04699166aa
25-ID-D user <s25idduser@lerix.xray.aps.anl.gov>
2023-03-28 14:43:53 -0500

    Fixed live plotting in ipython startup file.

--- ipython_startup.py
+++ ipython_startup.py
@@ -2,19 +2,20 @@ from bluesky import plans as bp, plan_stubs as bps, RunEngine, suspenders
 from bluesky.simulators import summarize_plan
 from bluesky.callbacks.best_effort import BestEffortCallback
 import databroker
-
-# Set up live-plotting
-# from bluesky.utils import install_qt_kicker
-# install_qt_kicker()
+import matplotlib.pyplot as plt
 
 import haven
+
+# Prepare the haven instrument
 config = haven.load_config()
 print(f"Initializing {config['beamline']['name']}...", end="", flush=True)
 haven.load_instrument()
 print("done")
 RE = haven.run_engine()
+ion_chambers = haven.registry.findall("ion_chambers")
 
 # Set up best effort callback for visualizing live data
+plt.ion()
 bec = BestEffortCallback()
 RE.subscribe(bec)
 
