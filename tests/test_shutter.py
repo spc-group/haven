ace16c05d9501cae90c5b9899c374b401e5cf632
Mark Wolfman <canismarko@gmail.com>
2023-04-02 14:19:52 -0500

    [MNT] Re-factored PSS shutter devices to require less configuration.

--- tests/test_shutter.py
+++ tests/test_shutter.py
@@ -5,7 +5,14 @@ from haven import registry
 def test_shutter():
     load_shutters()
     shutters = registry.findall(label="shutters")
-    assert len(shutters) == 1
-    shutterA = shutters[0]
-    assert shutterA.name == "Shutter A"
+    assert len(shutters) == 2
+    shutterA = registry.find(name="front_end_shutter")
+    assert shutterA.name == "front_end_shutter"
     assert shutterA.open_signal.pvname == "PSS:99ID:FES_OPEN_EPICS.VAL"
+    assert shutterA.close_signal.pvname == "PSS:99ID:FES_CLOSE_EPICS.VAL"
+    assert shutterA.pss_state.pvname == "PSS:99ID:A_BEAM_PRESENT"
+    shutterC = registry.find(name="hutch_shutter")
+    assert shutterC.name == "hutch_shutter"
+    assert shutterC.open_signal.pvname == "PSS:99ID:SCS_OPEN_EPICS.VAL"
+    assert shutterC.close_signal.pvname == "PSS:99ID:SCS_CLOSE_EPICS.VAL"
+    assert shutterC.pss_state.pvname == "PSS:99ID:C_BEAM_PRESENT"
