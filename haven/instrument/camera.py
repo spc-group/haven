fdfcf2b5f46fc5e98b64949fda3f2ef00440389c
Sector 25 ID-C User <s25idcuser@microprobe.xray.aps.anl.gov>
2023-04-11 15:08:47 -0500

    Added controls to the AD viewer window for all ROI's and overlays.

--- haven/instrument/camera.py
+++ haven/instrument/camera.py
@@ -4,16 +4,16 @@ from typing import Optional, Sequence
 
 from ophyd import (
     CamBase,
-    DetectorBase,
     SingleTrigger,
     Kind,
     ADComponent as ADCpt,
     EpicsSignal,
 )
-from ophyd.areadetector.plugins import ImagePlugin_V34, PvaPlugin_V34, OverlayPlugin
+from ophyd.areadetector.plugins import ImagePlugin_V34, PvaPlugin_V34, OverlayPlugin, ROIPlugin_V34
 
 
 from .instrument_registry import registry
+from .area_detector import DetectorBase, StatsPlugin_V34, SimDetector
 from .._iconfig import load_config
 
 
@@ -28,24 +28,28 @@ class AravisCam(CamBase):
     acquire_time_auto = ADCpt(EpicsSignal, "ExposureAuto")
 
 
-class Camera(SingleTrigger, DetectorBase):
+class AravisDetector(SingleTrigger, DetectorBase):
     """
     A gige-vision camera described by EPICS.
     """
-
-    def __init__(self, *args, description=None, **kwargs):
-        super().__init__(*args, **kwargs)
-        if description is None:
-            description = self.prefix
-        self.description = description
 
     cam = ADCpt(AravisCam, "cam1:")
     image = ADCpt(ImagePlugin_V34, "image1:")
     pva = ADCpt(PvaPlugin_V34, "Pva1:")
     overlays = ADCpt(OverlayPlugin, "Over1:")
+    roi1 = ADCpt(ROIPlugin_V34, "ROI1:", kind=Kind.config)
+    roi2 = ADCpt(ROIPlugin_V34, "ROI2:", kind=Kind.config)
+    roi3 = ADCpt(ROIPlugin_V34, "ROI3:", kind=Kind.config)
+    roi4 = ADCpt(ROIPlugin_V34, "ROI4:", kind=Kind.config)
+    stats1 = ADCpt(StatsPlugin_V34, "Stats1:", kind=Kind.normal)
+    stats2 = ADCpt(StatsPlugin_V34, "Stats2:", kind=Kind.normal)
+    stats3 = ADCpt(StatsPlugin_V34, "Stats3:", kind=Kind.normal)
+    stats4 = ADCpt(StatsPlugin_V34, "Stats4:", kind=Kind.normal)
+    stats5 = ADCpt(StatsPlugin_V34, "Stats5:", kind=Kind.normal)
+    
 
 
-def load_cameras(config=None) -> Sequence[Camera]:
+def load_cameras(config=None) -> Sequence[DetectorBase]:
     """Load cameras from config files and add to the registry.
 
     Returns
@@ -60,13 +64,19 @@ def load_cameras(config=None) -> Sequence[Camera]:
     devices = {k: v for (k, v) in config["camera"].items() if k.startswith("cam")}
     # Load each camera
     cameras = []
-    for key, device in devices.items():
-        cam = Camera(
-            prefix=f"{device['ioc']}:",
-            name=device["name"],
-            description=device.get("description"),
+    for key, cam_config in devices.items():
+        class_name = cam_config.get("device_class", "AravisDetector")
+        DeviceClass = globals().get(class_name)
+        # Check that it's a valid device class
+        if DeviceClass is None:
+            msg = f"camera.{name}.device_class={cam_config['device_class']}"
+            raise exceptions.UnknownDeviceConfiguration(msg)
+        device = DeviceClass(
+            prefix=f"{cam_config['ioc']}:",
+            name=cam_config["name"],
+            description=cam_config.get("description", cam_config["name"]),
             labels={"cameras"},
         )
-        registry.register(cam)
-        cameras.append(cam)
+        registry.register(device)
+        cameras.append(device)
     return cameras
