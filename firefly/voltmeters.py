83a1c9e3a1a0bc8b918543244799f9cc197ff0c0
Mark Wolfman <canismarko@gmail.com>
2023-04-12 16:05:05 -0500

    Fixed the PVs for the autocount and read dark current control in voltmeters window.

--- firefly/voltmeters.py
+++ firefly/voltmeters.py
@@ -16,20 +16,35 @@ log = logging.getLogger(__name__)
 class VoltmetersDisplay(display.FireflyDisplay):
     _ion_chamber_displays = []
 
+    # def __init__(
+    #     self,
+    #     args: Optional[Sequence] = None,
+    #     macros: Mapping = {},
+    #     **kwargs,
+    # ):
+    #     macros_ = macros.copy()
+    #     scaler_ioc =
+    #     macros_["SCALER"] == load_config()["ion_chamber"]["scaler"]["ioc"]
+    #     super().__init__(args=args, macros=macros, **kwargs)
+
     def customize_ui(self):
         # Delete existing voltmeter widgets
         for idx in reversed(range(self.voltmeters_layout.count())):
             self.voltmeters_layout.takeAt(idx).widget().deleteLater()
         # Add embedded displays for all the ion chambers
         try:
-            ion_chambers = haven.registry.findall(label="ion_chambers")
+            ion_chambers = list(haven.registry.findall(label="ion_chambers"))
         except haven.exceptions.ComponentNotFound as e:
             warnings.warn(str(e))
             log.warning(e)
             ion_chambers = []
+        scaler_prefix = "CPT NOT FOUND"            
         self._ion_chamber_displays = []
-        print(ion_chambers)
         for ic in sorted(ion_chambers, key=lambda c: c.ch_num):
+            # Get the scaler prefix for other macros
+            if "SCALER" not in self.macros().keys():
+                self._macros = dict(SCALER=ic.scaler_prefix, **self.macros())
+            # Create the display object
             disp = PyDMEmbeddedDisplay(parent=self)
             disp.macros = json.dumps({"IC": ic.name})
             disp.filename = "voltmeter.py"
