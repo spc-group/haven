acb341d06595a6816dc270b84056b5b0a31479c6
Mark Wolfman <canismarko@gmail.com>
2023-07-12 10:44:49 -0500

    Prepare PSO in aerotech stage.

--- src/haven/instrument/stage.py
+++ src/haven/instrument/stage.py
@@ -1,5 +1,14 @@
-from ophyd import Device, FormattedComponent as FCpt, EpicsMotor
-from ophyd import Component as Cpt, Signal
+from ophyd import (
+    Device,
+    FormattedComponent as FCpt,
+    EpicsMotor,
+    Component as Cpt,
+    Signal,
+    SignalRO,
+    Kind,
+    EpicsSignal,
+)
+from apstools.synApps.asyn import AsynRecord
 
 from .instrument_registry import registry
 from .._iconfig import load_config
@@ -50,19 +59,141 @@ def load_stages(config=None):
         )
 
 
-class AerotechFlyer(Device):
-    """An Aerotech stage that allows for Flyscanning.
+class AerotechFlyer(EpicsMotor):
+    """Allow an Aerotech stage to fly-scan via the Ophyd FlyerInterface.
 
-    Records a start and end position, step size and slew speed.
+    Set *start_position*, *stop_position*, and *step_size* in units of
+    the motor record (.EGU), and *dwell_time* in seconds. Then the
+    remaining components will be calculated accordingly.
+
+    All position or distance components are assumed to be in motor
+    record engineering units, unless preceded with "encoder_", in
+    which case they are in units of encoder pulses based on the
+    encoder resolution.
+
+    Parameters
+    ==========
+    axis
+      The label used by the aerotech controller to refer to this
+      axis. Examples include "@0" or "X".
+    encoder
+      The number of the encoder to track when fly-scanning with this
+      device.
+
+    Components
+    ==========
+    start_position
+      Where the fly scan begins
+    stop_position
+      Where the fly scan ends
+    step_size
+      How much space desired between points. Note this is not
+      guaranteed and may be adjusted to match the encoder resolution
+      of the stage.
+    dwell_time
+      How long to take, in seconds, moving between points.
+    slew_speed
+      How fast to move the stage. Calculated from the remaining
+      components.
+
     """
 
-    start_position = Cpt(Signal, name="start_position")
-    end_position = Cpt(Signal, name="end_position")
-    step_size = Cpt(Signal, name="step_size")
-    slew_speed = Cpt(Signal, name="slew_speed")
+    # Internal encoder in the Ensemble to track for flying
+    encoder: int
+
+    # Desired fly parameters
+    start_position = Cpt(Signal, name="start_position", kind=Kind.config)
+    end_position = Cpt(Signal, name="end_position", kind=Kind.config)
+    step_size = Cpt(Signal, name="step_size", kind=Kind.config)
+    dwell_time = Cpt(Signal, name="dwell_time", kind=Kind.config)
+
+    # Calculated signals
+    slew_speed = Cpt(Signal, kind=Kind.config)
+    taxi_start = Cpt(Signal, kind=Kind.config)
+    taxi_end = Cpt(Signal, kind=Kind.config)
+    encoder_step_size = Cpt(Signal, kind=Kind.config)
+
+    def __init__(self, *args, axis: str, encoder: int, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.axis = axis
+        self.encoder = encoder
+        # Set up auto-calculations for the flyer
+        self.start_position.subscribe(self._update_fly_params)
+
+    def _update_fly_params(self, *args, **kwargs):
+        """Calculate new fly-scan parameters based on signal values."""
+        print("To-do: calculate fly-scanning parameters.")
+
+    def send_command(self, cmd: str):
+        """Send commands directly to the aerotech ensemble controller.
+
+        Returns
+        =======
+        status
+          The Ophyd status object for this write.
+
+        """
+        status = self.parent.asyn.ascii_output.set(cmd)
+        return status
+
+    def enable_pso(self):
+        num_axis = 1
+        statuses = [
+            # Make sure the PSO control is off
+            self.send_command(f"PSOCONTROL {self.axis} RESET"),
+            # Set the output to occur from the I/O terminal on the
+            # controller
+            self.send_command(f"PSOOUTPUT {self.axis} CONTROL {num_axis}"),
+            # Set a pulse 10 us long, 20 us total duration, so 10 us
+            # on, 10 us off
+            self.send_command(f"PSOPULSE {self.axis} TIME 20,10"),
+            # Set the pulses to only occur in a specific window
+            self.send_command(f"PSOOUTPUT {self.axis} PULSE WINDOW MASK"),
+            # Set which encoder we will use.  3 = the MXH (encoder
+            # multiplier) input. For Ensemble lab, 6 is horizontal encoder
+            self.send_command(f"PSOTRACK {self.axis} INPUT {self.encoder}"),
+            # Set the distance between pulses in encoder counts
+            self.send_command(f"PSODISTANCE {self.axis} FIXED {self.encoder_step_size.get()}"),
+            # Which encoder is being used to calculate whether we are
+            # in the window.
+            self.send_command(f"PSOWINDOW {self.axis} {num_axis} INPUT {self.encoder}"),
+            # Calculate window function parameters. Must be in encoder
+            # counts, and is referenced from the stage location where
+            # we arm the PSO
+            self.send_command(
+                f"PSOWINDOW {self.axis} {num_axis} RANGE "
+                f"{self.taxi_start.get()},{self.taxi_end.get()}"
+            ),
+        ]
+
+        for status in statuses:
+            status.wait()
 
 
 class AerotechFlyStage(XYStage):
-    """A subclass of XY stage for the Aerotech to include a flyer."""
+    """An XY stage for an Aerotech stage with fly-scanning capabilities.
 
-    flyer = Cpt(AerotechFlyer, name="flyer", labels={"flyers"})
+    Parameters
+    ==========
+
+    pv_vert
+      The suffix to the PV for the vertical motor.
+    pv_horiz
+      The suffix to the PV for the horizontal motor.
+    """
+
+    vert = FCpt(
+        AerotechFlyer,
+        "{prefix}{pv_vert}",
+        axis="@1",
+        encoder=6,
+        labels={"motors", "flyers"},
+    )
+    horiz = FCpt(
+        AerotechFlyer,
+        "{prefix}{pv_horiz}",
+        axis="@0",
+        encoder=7,
+        labels={"motors", "flyers"},
+    )
+    asyn = Cpt(AsynRecord, ":asynEns", name="async", labels={"asyns"})
--- tests/test_stages.py
+++ tests/test_stages.py
@@ -1,3 +1,4 @@
+from unittest import mock
 import pytest
 
 from haven import registry, exceptions
@@ -30,13 +31,48 @@ def test_load_aerotech_stage():
     assert vert_ is not None
 
 
-def test_Aerotech_flyer():
-    aeroflyer = stage.AerotechFlyer(name="aerotech_flyer")
+def test_aerotech_flyer():
+    aeroflyer = stage.AerotechFlyer(name="aerotech_flyer", axis="@0", encoder=6)
     assert aeroflyer is not None
 
 
 def test_aerotech_stage():
-    flyer = stage.AerotechFlyStage(
+    fly_stage = stage.AerotechFlyStage(
         "motor_ioc", pv_vert=":m1", pv_horiz=":m2", labels={"stages"}, name="aerotech"
     )
-    assert flyer is not None
+    assert fly_stage is not None
+    assert fly_stage.asyn.ascii_output.pvname == "motor_ioc:asynEns.AOUT"
+
+
+def test_aerotech_slew_speed():
+    flyer = stage.AerotechFlyer(name="flyer", axis="@0", encoder=0)
+    # Set some example positions
+    flyer.motor_egu.set("micron").wait()
+    flyer.start_position.set(10).wait()
+    flyer.end_position.set(20).wait()
+    flyer.step_size.set(0.1).wait()
+    flyer.dwell_time.set(1).wait()
+    assert flyer.slew_speed.get(use_monitor=False) == 0.1
+
+
+def test_enable_pso():
+    flyer = stage.AerotechFlyer(name="flyer", axis="@0", encoder=6)
+    flyer.send_command = mock.MagicMock()
+    # Set up scan parameters
+    flyer.encoder_step_size.set(50).wait()   # In encoder counts
+    flyer.taxi_start.set(-0.025).wait()  # In encoder counts
+    flyer.taxi_end.set(9999.975).wait()  # In encoder counts
+    # Check that commands are sent to set up the controller for flying
+    flyer.enable_pso()
+    assert flyer.send_command.called
+    commands = [c.args[0] for c in flyer.send_command.call_args_list]
+    assert commands == [
+        "PSOCONTROL @0 RESET",
+        "PSOOUTPUT @0 CONTROL 1",
+        "PSOPULSE @0 TIME 20,10",
+        "PSOOUTPUT @0 PULSE WINDOW MASK",
+        "PSOTRACK @0 INPUT 6",
+        "PSODISTANCE @0 FIXED 50",
+        "PSOWINDOW @0 1 INPUT 6",
+        "PSOWINDOW @0 1 RANGE -0.025,9999.975",
+    ]
