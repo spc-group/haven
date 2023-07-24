Fixed an off-by-one error when calculation PSO positions.

src/haven/instrument/stage.py
@@ -364,7 +364,8 @@ class AerotechFlyer(EpicsMotor, flyers.FlyerInterface):
         taxi_start =  pso_start - (direction * taxi_dist)
         taxi_end =  pso_end + (direction * taxi_dist)
         #Create a list of PSO positions and grab the end to find end of scan
-        pso_positions = np.arange(start_position, end_position, (direction*step_size))
+        pso_positions_step = direction*step_size
+        pso_positions = np.arange(start_position, end_position+0.5*pso_positions_step, pso_positions_step)
         # Calculate encoder counts within the requested window of the scan
         encoder_window_start = int(-direction * window_buffer)
         # import pdb; pdb.set_trace()

tests/test_stages.py
@@ -81,7 +81,7 @@ def test_aerotech_fly_params_forward(sim_aerotech_flyer):
     assert flyer.encoder_window_end.get(use_monitor=False) == 10005
     i = 10.05
     pso = []
-    while i<=19.95:
+    while i<=19.98:
         pso.append(i)
         i = i+0.1
     np.testing.assert_allclose(flyer.pso_positions, pso)
@@ -110,7 +110,7 @@ def test_aerotech_fly_params_reverse(sim_aerotech_flyer):
     
     i = 19.95
     pso = []
-    while i>=10.05:
+    while i>=10.03:
         pso.append(i)
         i = i-0.1
     np.testing.assert_allclose(flyer.pso_positions, pso)