cfbec2912261c1b892c1a865960af64c80b60409
Mark Wolfman <canismarko@gmail.com>
2023-11-24 21:20:12 -0600

    Added signals for tweak values to the slits device.
    Also added a resource for the KB mirror 3D diagram.

--- src/haven/instrument/slits.py
+++ src/haven/instrument/slits.py
@@ -1,13 +1,71 @@
+"""This is a copy of the apstools Slits support with signals for the tweak PV."""
+
+
 import asyncio
 import logging
 
+from ophyd import FormattedComponent as FCpt, Component as Cpt
+from ophyd import Device
+from ophyd import EpicsSignal
 from apstools.synApps.db_2slit import Optics2Slit2D_HV
+from apstools.devices import PVPositionerSoftDone
+from apstools.utils import SlitGeometry
 
 from .._iconfig import load_config
 from .device import aload_devices, await_for_connection, make_device
 from .instrument_registry import registry
 
 log = logging.getLogger(__name__)
+
+
+class PVPositionerWithTweaks(PVPositionerSoftDone):
+    tweak_value = FCpt(EpicsSignal, "{prefix}{_setpoint_pv}_tweakVal.VAL")
+    tweak_forward = FCpt(EpicsSignal, "{prefix}{_setpoint_pv}_tweak.B")
+    tweak_reverse = FCpt(EpicsSignal, "{prefix}{_setpoint_pv}_tweak.A")
+
+
+class Optics2Slit1D(Device):
+    """
+    EPICS synApps optics 2slit.db 1D support: xn, xp, size, center, sync
+
+    "sync" is used to tell the EPICS 2slit database to synchronize the
+    virtual slit values with the actual motor positions.
+    """
+
+    xn = Cpt(PVPositionerWithTweaks, "", setpoint_pv="xn", readback_pv="t2.B")
+    xp = Cpt(PVPositionerWithTweaks, "", setpoint_pv="xp", readback_pv="t2.A")
+    size = Cpt(PVPositionerWithTweaks, "", setpoint_pv="size", readback_pv="t2.C")
+    center = Cpt(PVPositionerWithTweaks, "", setpoint_pv="center", readback_pv="t2.D")
+
+    sync = Cpt(EpicsSignal, "sync", put_complete=True, kind="omitted")
+
+
+class Optics2Slit2D_HV(Device):
+    """
+    EPICS synApps optics 2slit.db 2D support: h.xn, h.xp, v.xn, v.xp
+    """
+
+    h = Cpt(Optics2Slit1D, "H")
+    v = Cpt(Optics2Slit1D, "V")
+
+    @property
+    def geometry(self):
+        """Return the slit 2D size and center as a namedtuple."""
+        pppp = [
+            round(obj.position, obj.precision) for obj in (self.h.size, self.v.size, self.h.center, self.v.center)
+        ]
+
+        return SlitGeometry(*pppp)
+
+    @geometry.setter
+    def geometry(self, value):
+        # first, test the input by assigning it to local vars
+        width, height, x, y = value
+
+        self.h.size.move(width)
+        self.v.size.move(height)
+        self.h.center.move(x)
+        self.v.center.move(y)
 
 
 async def make_slits_device(prefix, name):
--- src/haven/tests/test_slits.py
+++ src/haven/tests/test_slits.py
@@ -3,10 +3,21 @@ from unittest import mock
 from haven.instrument import slits
 
 
+def test_slits_tweak():
+    """Test the PVs for the tweak forward/reverse."""
+    slits_obj = slits.Optics2Slit2D_HV("255idc:KB_slits", name="KB_slits")
+    # Check the inherited setpoint/readback PVs
+    assert slits_obj.v.center.setpoint.pvname == "255idc:KB_slitsVcenter"
+    assert slits_obj.v.center.readback.pvname == "255idc:KB_slitsVt2.D"
+    # Check the tweak PVs
+    assert slits_obj.v.center.tweak_value.pvname == "255idc:KB_slitsVcenter_tweakVal.VAL"
+    assert slits_obj.v.center.tweak_reverse.pvname == "255idc:KB_slitsVcenter_tweak.A"
+    assert slits_obj.v.center.tweak_forward.pvname == "255idc:KB_slitsVcenter_tweak.B"
+
+
 def test_load_slits(sim_registry, monkeypatch):
     monkeypatch.setattr(slits, "await_for_connection", mock.AsyncMock())
     slits.load_slits()
     # Check that the slits were loaded
     device = sim_registry.find(label="slits")
     assert device.prefix == "vme_crate_ioc:KB"
-    assert device.h.center.setpoint.pvname == "vme_crate_ioc:KBHcenter"
