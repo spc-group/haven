a8f51018e141fe101e6a0313e8e7d2047e841146
Juanjuan Huang <juanjuan.huang@goldendale.xray.aps.anl.gov>
2023-12-04 17:57:24 -0600

    renamed plan_1D_scan to line_scan, and added the bluesky scan plan

---
+++ src/firefly/plans/line_scan.py
@@ -0,0 +1,35 @@
+import logging
+
+from bluesky_queueserver_api import BPlan
+
+from firefly import display
+
+log = logging.getLogger()
+
+
+class CountDisplay(display.FireflyDisplay):
+    def customize_ui(self):
+        # self.ui.run_button.setEnabled(True) for testing
+        self.ui.run_button.clicked.connect(self.queue_plan)
+
+    def queue_plan(self, *args, **kwargs):
+        """Execute this plan on the queueserver."""
+        # Get scan parameters from widgets
+        detectors = self.ui.detectors_list.selected_detectors()
+        motor = self.ui.motorA_comboBox.currentText()
+        start = float(self.ui.scan_start_lineEdit.text())
+        stop = float(self.ui.scan_stop_lineEdit.text())
+        num_points = self.ui.scan_pts_spin_box.value()
+
+        # # Build the queue item
+        item = BPlan("scan", detectors, motor, start, stop, num=num_points, per_step=None, md=None)
+
+        # Submit the item to the queueserver
+        from firefly.application import FireflyApplication
+
+        app = FireflyApplication.instance()
+        log.info("Add ``scan()`` plan to queue.")
+        app.add_queue_item(item)
+
+    def ui_filename(self):
+        return "plans/line_scan.ui"
