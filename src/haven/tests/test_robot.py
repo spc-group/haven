e73b83151cedb39189aba3fe6baebbbb70f95e33
Yanna Chen <chen.yanna2022@gmail.com>
2024-02-23 20:57:20 -0600

    Reformat test file

--- src/haven/tests/test_robot.py
+++ src/haven/tests/test_robot.py
@@ -22,7 +22,7 @@ def test_load_robot(sim_registry):
 
 def test_load_no_robot(sim_registry):
     load_robot(config={})
-    
+
     # Test the robot is not in config
     result = pytest.raises(ComponentNotFound, sim_registry.findall, label="robots")
 
@@ -30,6 +30,7 @@ def test_load_no_robot(sim_registry):
     assert 'Could not find components matching: label="robots", name="None"' in str(
         result.value
     )
+
 
 # -----------------------------------------------------------------------------
 # :author:    Yanna Chen
