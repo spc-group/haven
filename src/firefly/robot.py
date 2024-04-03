935f6bab09498ff04292507af7990f10b815b715
Yanna Chen <chen.yanna2022@gmail.com>
2024-04-03 16:09:46 -0500

    Add motor list in GUI

--- src/firefly/robot.py
+++ src/firefly/robot.py
@@ -1,9 +1,10 @@
 import logging
 
 import bluesky_queueserver_api import BPlan
-
+from qtpy import QtWidgets
 from firefly import display
 from firefly.application import FireflyApplication
+from firefly.component_selector import ComponentSelector
 
 from have import exceptions, load_config, registry
 
@@ -11,21 +12,95 @@ log = logging.getLogger(__name__)
 
 SAMPLE_NUMBERS = range(24)
 
+class LineScanRegion:
+    def __init__(self):
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.layout = QtWidgets.QHBoxLayout()
+
+        # First item, motor No.
+        # self.motor_label = QtWidgets.QLabel()
+        # self.motor_label.setText("1")
+        # self.layout.addWidget(self.motor_label)
+
+        # Second item, ComponentSelector
+        self.motor_box = ComponentSelector()
+        # self.stop_line_edit.setPlaceholderText("Stop…")
+        self.layout.addWidget(self.motor_box)
+
+        # Third item, start point
+        self.start_line_edit = QtWidgets.QLineEdit()
+        self.start_line_edit.setPlaceholderText("Start…")
+        self.layout.addWidget(self.start_line_edit)
+
+        # Forth item, stop point
+        self.stop_line_edit = QtWidgets.QLineEdit()
+        self.stop_line_edit.setPlaceholderText("Stop…")
+        self.layout.addWidget(self.stop_line_edit)
+
+
+
+
 class RobotDisplay(display.FireflyDisplay):
     """
     A GUI for managing sample tranfer using a robot
     """
 
     def customize_ui(self):
+        # disable the line edits in spin box
+        self.ui.num_motor_spin_box.lineEdit().setReadOnly(True)
+        self.ui.num_motor_spin_box.valueChanged.connect(self.update_regions)
+        # self.ui.num_motor_spin_box.editingFinished.connect(self.update_regions)
 
+        # self.ui.run_button.setEnabled(True) #for testing
+        self.ui.run_button.clicked.connect(self.queue_plan)
+    
+    def add_regions(self, num=1):
+        for i in range(num):
+            region = LineScanRegion()
+            self.ui.regions_layout.addLayout(region.layout)
+            # Save it to the list
+            self.regions.append(region)
 
-    def transfer_sample(self, number, *args, **kwargs):
+    def remove_regions(self, num=1):
+        for i in range(num):
+            layout = self.regions[-1].layout
+            # iterate/wait, and delete all widgets in the layout in the end
+            while layout.count():
+                item = layout.takeAt(0)
+                if item.widget():
+                    item.widget().deleteLater()
+            self.regions.pop()
+    
+    def update_regions(self):
+        new_region_num = self.ui.num_motor_spin_box.value()
+        old_region_num = len(self.regions)
+        diff_region_num = new_region_num - old_region_num
+
+        if diff_region_num < 0:
+            self.remove_regions(abs(diff_region_num))
+        elif diff_region_num > 0:
+            self.add_regions(diff_region_num)
+    
+    def queue_plan(self, *args, **kwargs):
+        """Execute this plan on the queueserver."""
+        # Get scan parameters from widgets
+        num_readings = self.ui.num_spinbox.value()
+        delay = self.ui.delay_spinbox.value()
+        detectors = self.ui.detectors_list.selected_detectors()
+        # Build the queue item
+
+        item = BPlan("count", delay=delay, num=num_readings, detectors=detectors)
+        # Submit the item to the queueserver
+        from firefly.application import FireflyApplication
+
+        app = FireflyApplication.instance()
+        log.info("Add ``count()`` plan to queue.")
+        app.add_queue_item(item)
 
     def ui_filename(self):
         return "robot.ui"
-
-
-
 # -----------------------------------------------------------------------------
 # :author:    Yanna Chen
 # :email:     yannachen@anl.gov
