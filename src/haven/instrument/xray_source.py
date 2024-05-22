3d9720c59d6d8f33a3272c6fd8d880fdc67425e6
Mark Wolfman <canismarko@gmail.com>
2024-05-22 11:51:44 -0500

    Added an APSUndulator device with new PVs (derived signals don't work).

--- src/haven/instrument/xray_source.py
+++ src/haven/instrument/xray_source.py
@@ -1,12 +1,83 @@
 import logging
 import warnings
+from enum import IntEnum
 
 from apstools.devices.aps_undulator import ApsUndulator
+from ophyd import EpicsSignal, EpicsSignalRO, DerivedSignal, Component as Cpt, PVPositioner, Device
 
 from .._iconfig import load_config
 from .device import make_device
 
 log = logging.getLogger(__name__)
+
+
+class DoneStatus(IntEnum):
+    MOVING = 0
+    DONE = 1
+
+
+class BusyStatus(IntEnum):
+    DONE = 0
+    BUSY = 1
+
+
+class MotorDriveStatus(IntEnum):
+    NOT_READY = 0
+    READY_TO_MOVE = 1
+
+
+class UndulatorPositioner(PVPositioner):
+    setpoint = Cpt(EpicsSignal, "SetC.VAL")
+    readback = Cpt(EpicsSignalRO, "M.VAL")
+
+    actuate = Cpt(DerivedSignal, "parent.start_button", kind="omitted")
+    stop_signal = Cpt(DerivedSignal, "parent.stop_button", kind="omitted")
+    put_complete = 1
+    done = Cpt(DerivedSignal, "parent.done", kind="omitted")
+    done_value = DoneStatus.DONE
+
+
+class PlanarUndulator(Device):
+    """APS Planar Undulator
+
+    .. index:: Ophyd Device; PlanarUndulator
+
+    The signals *busy* and *done* convey complementary
+    information. *busy* comes from the IOC, while *done* comes
+    directly from the controller.
+
+    EXAMPLE::
+
+        undulator = PlanarUndulator("S25ID:USID:", name="undulator")
+
+    """
+    energy = Cpt(UndulatorPositioner, "Energy")
+    energy_taper = Cpt(UndulatorPositioner, "TaperEnergy")
+    gap = Cpt(PVPositioner, "Gap")
+    gap_taper = Cpt(UndulatorPositioner, "TaperGap")
+
+    start_button = Cpt(EpicsSignal, "StartC.VAL", put_complete=True, kind="omitted")
+    stop_button = Cpt(EpicsSignal, "StopC.VAL", kind="omitted")
+    busy = Cpt(EpicsSignalRO, "BusyM.VAL", kind="omitted")
+    done = Cpt(EpicsSignalRO, "BusyDeviceM.VAL", kind="omitted")
+    motor_drive_status = Cpt(EpicsSignalRO, "MotorDriveStatusM.VAL", kind="omitted")
+
+    harmonic_value = Cpt(EpicsSignal, "HarmonicValueC", kind="config")
+    gap_deadband = Cpt(EpicsSignal, "DeadbandGapC", kind="config")
+    device_limit = Cpt(EpicsSignal, "DeviceLimitM.VAL", kind="config")
+
+    access_mode = Cpt(EpicsSignalRO, "AccessSecurityC", kind="omitted")
+    total_power = Cpt(EpicsSignalRO, "TotalPowerM.VAL", kind="config")
+    message1 = Cpt(EpicsSignalRO, "Message1M.VAL", kind="omitted")
+    message2 = Cpt(EpicsSignalRO, "Message2M.VAL", kind="omitted")
+    message3 = Cpt(EpicsSignalRO, "Message3M.VAL", kind="omitted")
+    # time_left = Cpt(EpicsSignalRO, "ShClosedTime", kind="omitted")
+
+    device = Cpt(EpicsSignalRO, "DeviceM", kind="config")
+    magnet = Cpt(EpicsSignalRO, "DeviceMagnetM", kind="config")
+    location = Cpt(EpicsSignalRO, "LocationM", kind="config")
+    version_plc = Cpt(EpicsSignalRO, "PLCVersionM.VAL", kind="config")
+    version_hpmu = Cpt(EpicsSignalRO, "HPMUVersionM.VAL", kind="config")
 
 
 def load_xray_source(config=None):
