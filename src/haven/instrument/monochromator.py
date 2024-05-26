b029a339f0fb32fc60ac4ed9df6d47e1e95cb948
Mark Wolfman <canismarko@gmail.com>
2024-05-25 20:29:14 -0500

    Updated Monochromator device so that the prefix now includes the trailing ':'.

--- src/haven/instrument/monochromator.py
+++ src/haven/instrument/monochromator.py
@@ -17,33 +17,33 @@ class IDTracking(IntEnum):
 
 class Monochromator(Device):
     # ID tracking PVs
-    id_tracking = Cpt(EpicsSignal, ":ID_tracking", kind="config")
-    id_offset = Cpt(EpicsSignal, ":ID_offset", kind="config")
-    d_spacing = Cpt(EpicsSignal, ":dspacing", kind="config")
+    id_tracking = Cpt(EpicsSignal, "ID_tracking", kind="config")
+    id_offset = Cpt(EpicsSignal, "ID_offset", kind="config")
+    d_spacing = Cpt(EpicsSignal, "dspacing", kind="config")
     # Virtual positioners
-    mode = Cpt(EpicsSignal, ":mode", labels={"motors", "baseline"}, kind="config")
-    energy = Cpt(EpicsMotor, ":Energy", labels={"motors"}, kind="hinted")
+    mode = Cpt(EpicsSignal, "mode", labels={"motors", "baseline"}, kind="config")
+    energy = Cpt(EpicsMotor, "Energy", labels={"motors"}, kind="hinted")
     energy_constant1 = Cpt(
-        EpicsSignal, ":EnergyC1.VAL", labels={"baseline"}, kind="config"
+        EpicsSignal, "EnergyC1.VAL", labels={"baseline"}, kind="config"
     )
     energy_constant2 = Cpt(
-        EpicsSignal, ":EnergyC2.VAL", labels={"baseline"}, kind="config"
+        EpicsSignal, "EnergyC2.VAL", labels={"baseline"}, kind="config"
     )
     energy_constant3 = Cpt(
-        EpicsSignal, ":EnergyC3.VAL", labels={"baseline"}, kind="config"
+        EpicsSignal, "EnergyC3.VAL", labels={"baseline"}, kind="config"
     )
-    offset = Cpt(EpicsMotor, ":Offset", labels={"motors", "baseline"}, kind="config")
+    offset = Cpt(EpicsMotor, "Offset", labels={"motors", "baseline"}, kind="config")
     # ACS Motors
-    horiz = Cpt(EpicsMotor, ":ACS:m1", labels={"motors", "baseline"}, kind="config")
-    vert = Cpt(EpicsMotor, ":ACS:m2", labels={"motors", "baseline"}, kind="config")
-    bragg = Cpt(EpicsMotor, ":ACS:m3", labels={"motors"})
-    gap = Cpt(EpicsMotor, ":ACS:m4", labels={"motors"})
-    roll2 = Cpt(EpicsMotor, ":ACS:m5", labels={"motors", "baseline"}, kind="config")
-    pitch2 = Cpt(EpicsMotor, ":ACS:m6", labels={"motors", "baseline"}, kind="config")
-    roll_int = Cpt(EpicsMotor, ":ACS:m7", labels={"motors", "baseline"}, kind="config")
-    pi_int = Cpt(EpicsMotor, ":ACS:m8", labels={"motors", "baseline"}, kind="config")
+    horiz = Cpt(EpicsMotor, "ACS:m1", labels={"motors", "baseline"}, kind="config")
+    vert = Cpt(EpicsMotor, "ACS:m2", labels={"motors", "baseline"}, kind="config")
+    bragg = Cpt(EpicsMotor, "ACS:m3", labels={"motors"})
+    gap = Cpt(EpicsMotor, "ACS:m4", labels={"motors"})
+    roll2 = Cpt(EpicsMotor, "ACS:m5", labels={"motors", "baseline"}, kind="config")
+    pitch2 = Cpt(EpicsMotor, "ACS:m6", labels={"motors", "baseline"}, kind="config")
+    roll_int = Cpt(EpicsMotor, "ACS:m7", labels={"motors", "baseline"}, kind="config")
+    pi_int = Cpt(EpicsMotor, "ACS:m8", labels={"motors", "baseline"}, kind="config")
     # Physical constants
-    d_spacing = Cpt(EpicsSignalRO, ":dspacing", labels={"baseline"}, kind="config")
+    d_spacing = Cpt(EpicsSignalRO, "dspacing", labels={"baseline"}, kind="config")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
