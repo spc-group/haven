ef5ac128bfbfd2bcbfca90832be0653908916b9a
Mark Wolfman <canismarko@gmail.com>
2024-08-08 11:43:35 -0500

    Added a basic pydm plugin for ophyd-async.

--- src/firefly/pydm_plugin.py
+++ src/firefly/pydm_plugin.py
@@ -6,12 +6,23 @@ Provides funcitonality so that PyDM channels can be addressed as e.g.
 
 """
 
+import asyncio
+import logging
+from typing import Mapping
+
+from bluesky.protocols import Movable
+from pydm.data_plugins.plugin import PyDMConnection
+from ophyd.utils.epics_pvs import AlarmSeverity
+import numpy as np
+from qasync import asyncSlot
 from typhos.plugins.core import SignalConnection, SignalPlugin
 
 from haven import registry
 
+log = logging.getLogger(__name__)
 
-class HavenConnection(SignalConnection):
+
+class RegistryConnection:
     def find_signal(self, address: str):
         """Find a signal in the registry given its address.
         This method is intended to be overridden by subclasses that
@@ -30,6 +41,95 @@ class HavenConnection(SignalConnection):
         return registry[address]
 
 
+class HavenConnection(RegistryConnection, SignalConnection):
+    pass
+
+
+class HavenAsyncConnection(RegistryConnection, PyDMConnection):
+    def __init__(self, channel, address, protocol=None, parent=None):
+        print(f"Creating connection {self}")
+        # Create base connection
+        super().__init__(channel, address, protocol=protocol, parent=parent)
+        self._connection_open = True
+        self.signal_type = None
+        self.is_float = False
+        # Collect our signal
+        self.signal = self.find_signal(address)
+        # Subscribe to updates from Ophyd
+        self.signal.subscribe(self.send_new_value)
+        # Add listener
+        self.add_listener(channel)
+
+    def add_listener(self, channel):
+        super().add_listener(channel)
+        print("ADDING LISTENER")
+        # If the channel is used for writing to PVs, hook it up to the 'put' methods.
+        if channel.value_signal is not None:
+            for type_ in [str, int, float, np.ndarray]:
+                try:
+                    channel.value_signal[type_].connect(self.put_value)
+                except KeyError:
+                    pass
+        # Emit updated value/metadata so the new channel gets notified
+        self._meta_task = asyncio.create_task(
+            self.send_new_meta(), name=f"meta_{self.signal.name}"
+        )
+        self.signal._get_cache()._notify(self.send_new_value, want_value=False)
+
+    async def send_new_meta(self):
+        description = await self.signal.describe()
+        description = description[self.signal.name]
+        # Assume the signal is connected
+        self.connection_state_signal.emit(True)
+        # Check the bluesky interface for writability
+        is_writable = isinstance(self.signal, Movable)
+        self.write_access_signal.emit(is_writable)
+        # What is the precision of this signal
+        if (precision := description.get("precision")) is not None:
+            self.prec_signal.emit(precision)
+        # What are the units?
+        if (units := description.get("units")) is not None:
+            self.units_signal.emit(units)
+        # Update choices for enumerated types
+        if (enum_strs := description.get("choices")) is not None:
+            self.enum_strings_signal.emit(enum_strs)
+
+    def send_new_value(self, reading: Mapping = {}, **kwargs):
+        """
+        Update the UI with a new value from the Signal.
+        """
+        reading = reading[self.signal.name]
+        # Update value
+        value = reading["value"]
+        try:
+            self.new_value_signal.emit(value)
+        except Exception:
+            log.exception("Unable to update %r with value %r.", self.signal.name, value)
+        # Update alarm severity
+        severity = reading.get("alarm_severity", AlarmSeverity.NO_ALARM)
+        self.new_severity_signal.emit(severity)
+
+    def close(self):
+        """Unsubscribe from the Ophyd signal."""
+        self.signal.clear_sub(self.send_new_value)
+        self.signal.clear_sub(self.send_new_meta)
+
+    @asyncSlot(int)
+    @asyncSlot(float)
+    @asyncSlot(str)
+    @asyncSlot(np.ndarray)
+    async def put_value(self, new_value):
+        old_value = await self.signal.get_value()
+        print(f"PUTTING NEW VALUE: {old_value} -> {new_value}")
+        await self.signal.set(new_value)
+        print(f"Value for {self.signal} is now {await self.signal.get_value()}")
+
+
 class HavenPlugin(SignalPlugin):
     protocol = "haven"
     connection_class = HavenConnection
+
+
+class HavenAsyncPlugin(SignalPlugin):
+    protocol = "ahaven"
+    connection_class = HavenAsyncConnection
--- src/firefly/tests/test_pydm_plugin.py
+++ src/firefly/tests/test_pydm_plugin.py
@@ -1,7 +1,14 @@
+import uuid
+import asyncio
+from unittest.mock import MagicMock
+
+import pytest
 import pydm
 from ophyd import Signal
+from ophyd_async.core import SignalRW, soft_signal_rw
 from pydm.data_plugins import plugin_for_address
-from pydm.widgets import PyDMLineEdit
+from pydm.widgets import PyDMLineEdit, PyDMChannel
+from qtpy.QtCore import Signal as QSignal, QObject
 
 from firefly.pydm_plugin import HavenPlugin
 
@@ -29,3 +36,119 @@ def test_signal_connection(qapp, qtbot, sim_registry):
     assert widget._write_access
     assert widget._connected
     assert widget.value == 1
+
+
+@pytest.fixture()
+async def async_signal(sim_registry):
+    # Get a unique name so we don't recycle channels
+    signal_name = str(uuid.uuid4())
+    # Create the signal
+    sig = soft_signal_rw(float, initial_value=1.0, name=signal_name)
+    await sig.connect(mock=False)
+    sim_registry.register(sig)
+    return sig
+
+
+# @pytest.fixture()
+# async def async_widget(qapp, qtbot, async_signal):
+#     widget = PyDMLineEdit()
+#     qtbot.addWidget(widget)
+#     widget.channel = "ahaven://my_editable_signal"
+#     # Wait for metadata task to complete
+#     try:
+#         task = [t for t in asyncio.all_tasks() if t.get_name() == f"meta_my_signal"][0]
+#     except IndexError:
+#         # Connection already established
+#         pass
+#     else:
+#         await task
+#     qapp.processEvents()
+#     return widget
+
+
+@pytest.fixture()
+async def async_channel(qapp, qtbot, async_signal):
+
+    class SigHolder(QObject):
+        send_value = QSignal(float)
+
+    sigs = SigHolder()
+    channel = PyDMChannel(
+        address=f"ahaven://{async_signal.name}",
+        value_signal=sigs.send_value,
+        write_access_slot=MagicMock(),
+        connection_slot=MagicMock(),
+        value_slot=MagicMock(),
+    )
+    channel.connect()
+    # Wait for metadata task to complete
+    try:
+        task = [t for t in asyncio.all_tasks() if t.get_name() == f"meta_{async_signal.name}"][0]
+    except IndexError:
+        # Connection already established
+        pass
+    else:
+        await task
+    # Execute the test code
+    try:
+        yield channel
+    finally:
+        channel.disconnect()
+
+
+@pytest.mark.asyncio
+async def test_async_signal_connection(async_channel, async_signal):
+    # Check that our widget receives the initial values from the signal
+    channel = async_channel
+    assert channel.write_access_slot.called
+    assert channel.connection_slot.called
+    channel.value_slot.assert_called_once_with(1)
+    # Check that a second widget gets initial values
+    # channel2 = PyDMChannel(
+    #     address=f"ahaven://{async_signal.name}",
+    #     write_access_slot=MagicMock(),
+    #     connection_slot=MagicMock(),
+    #     value_slot=MagicMock(),
+    # )
+    # channel2.connect()
+    # task = [t for t in asyncio.all_tasks() if t.get_name() == f"meta_{async_signal.name}"][0]
+    # await task
+    # assert channel2.write_access_slot.called
+    # assert channel2.connection_slot.called
+    # channel2.value_slot.assert_called_once_with(1)
+
+
+@pytest.mark.asyncio
+async def test_async_send_value(qtbot, qapp, async_channel, async_signal):
+    """Can we update the ophyd signal from the channel?"""
+    channel = async_channel
+    signal = async_signal
+    # Check our starting state
+    assert (await signal.get_value()) == 1.0
+    # Update the widget
+    channel.value_signal.emit(2.0)
+    # qapp.processEvents()
+    # Fake test to see what's broken
+    # ch = widget.channels()[0]
+    # ch.value_signal[str].emit("2.0")
+    # await asyncio.gather(*asyncio.all_tasks())
+    for task in asyncio.all_tasks():
+        try:
+            await task
+        except RuntimeError:
+            pass
+    # Check the updated value
+    assert (await signal.get_value()) == 2.0
+
+
+@pytest.mark.asyncio
+async def test_async_gets_value(qtbot, qapp, async_channel, async_signal):
+    """Can we update the channel from the ophyd signal?"""
+    channel = async_channel
+    signal = async_signal
+    channel.value_slot.reset_mock()
+    # Update the ophyd signal
+    await signal.set(3.0)
+    qapp.processEvents()
+    # Check that the channel slot was updated
+    channel.value_slot.assert_called_once_with(3.0)
