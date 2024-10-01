dfb15d43e872bfdfcad21d00ca50a5be5e378d2e
Juanjuan Huang <juanjuan.cathy@gmail.com>
2024-10-01 11:38:23 -0500

    add fake position run into a seperate python file so it can be used for multiple pytests

--- src/haven/tests/test_save_motor_positions.py
+++ src/haven/tests/test_save_motor_positions.py
@@ -1,10 +1,8 @@
 import datetime as dt
 import logging
-from datetime import datetime
 from unittest.mock import MagicMock
 from zoneinfo import ZoneInfo
 
-import pandas as pd
 import pytest
 import time_machine
 from ophyd_async.core import set_mock_value
@@ -12,6 +10,7 @@ from tiled.adapters.mapping import MapAdapter
 from tiled.adapters.xarray import DatasetAdapter
 from tiled.client import Context, from_context
 from tiled.server.app import build_app
+from firefly.fake_position_runs import position_runs
 
 from haven.instrument import Motor
 from haven.motor_position import (
@@ -27,167 +26,6 @@ log = logging.getLogger(__name__)
 
 # Use a timezone we're not likely to be in for testing tz-aware behavior
 fake_time = dt.datetime(2022, 8, 19, 19, 10, 51, tzinfo=ZoneInfo("Asia/Taipei"))
-
-
-position_runs = {
-    "a9b3e0fa-eba1-43e0-a38c-c7ac76278000": MapAdapter(
-        {
-            "primary": MapAdapter(
-                {
-                    "data": DatasetAdapter.from_dataset(
-                        pd.DataFrame(
-                            {
-                                "motor_A": [12.0],
-                                "motor_B": [-113.25],
-                            }
-                        ).to_xarray()
-                    ),
-                },
-                metadata={
-                    "descriptors": [
-                        {
-                            "data_keys": {
-                                "motor_A": {"object_name": "motor_A"},
-                                "motor_B": {"object_name": "motor_B"},
-                            },
-                        }
-                    ],
-                },
-            ),
-        },
-        metadata={
-            "plan_name": "save_motor_position",
-            "position_name": "Good position A",
-            "time": 1725897133,
-            "uid": "a9b3e0fa-eba1-43e0-a38c-c7ac76278000",
-            "start": {
-                "plan_name": "save_motor_position",
-                "position_name": "Good position A",
-                "time": 1725897133,
-                "uid": "a9b3e0fa-eba1-43e0-a38c-c7ac76278000",
-            },
-        },
-    ),
-    # A second saved motor position
-    "1b7f2ef5-6a3c-496e-9f6f-f1a4805c0065": MapAdapter(
-        {
-            "primary": MapAdapter(
-                {
-                    "data": DatasetAdapter.from_dataset(
-                        pd.DataFrame(
-                            {
-                                "motorC": [11250.0],
-                            }
-                        ).to_xarray()
-                    ),
-                },
-                metadata={
-                    "descriptors": [
-                        {
-                            "data_keys": {
-                                "motorC": {"object_name": "motorC"},
-                            },
-                        }
-                    ],
-                },
-            ),
-        },
-        metadata={
-            "plan_name": "save_motor_position",
-            "position_name": "A good position B",
-            "time": 1725897193,
-            "uid": "1b7f2ef5-6a3c-496e-9f6f-f1a4805c0065",
-            "start": {
-                "plan_name": "save_motor_position",
-                "position_name": "A good position B",
-                "time": 1725897193,
-                "uid": "1b7f2ef5-6a3c-496e-9f6f-f1a4805c0065",
-            },
-        },
-    ),
-    # A saved motor position, but older
-    "5dd9a185-d5c4-4c8b-a719-9d7beb9007dc": MapAdapter(
-        {
-            "primary": MapAdapter(
-                {
-                    "data": DatasetAdapter.from_dataset(
-                        pd.DataFrame(
-                            {
-                                "motorC": [11250.0],
-                            }
-                        ).to_xarray()
-                    ),
-                },
-                metadata={
-                    "descriptors": [
-                        {
-                            "data_keys": {
-                                "motorC": {"object_name": "motorC"},
-                            },
-                        }
-                    ],
-                },
-            ),
-        },
-        metadata={
-            "plan_name": "save_motor_position",
-            "position_name": "Good position C",
-            "time": 1725897033,
-            "uid": "5dd9a185-d5c4-4c8b-a719-9d7beb9007dc",
-            "start": {
-                "plan_name": "save_motor_position",
-                "position_name": "Good position C",
-                "time": 1725897033,
-                "uid": "5dd9a185-d5c4-4c8b-a719-9d7beb9007dc",
-            },
-        },
-    ),
-    # A saved motor position, but older
-    "42b8c45d-e98d-4f59-9ce8-8f14134c90bd": MapAdapter(
-        {
-            "primary": MapAdapter(
-                {
-                    "data": DatasetAdapter.from_dataset(
-                        pd.DataFrame(
-                            {
-                                "motorC": [11250.0],
-                            }
-                        ).to_xarray()
-                    ),
-                },
-                metadata={
-                    "descriptors": [
-                        {
-                            "data_keys": {
-                                "motorC": {"object_name": "motorC"},
-                            },
-                        }
-                    ],
-                },
-            ),
-        },
-        metadata={
-            "plan_name": "save_motor_position",
-            "position_name": "Another good position",
-            "time": 1725897233,
-            "uid": "42b8c45d-e98d-4f59-9ce8-8f14134c90bd",
-            "start": {
-                "plan_name": "save_motor_position",
-                "position_name": "Another good position",
-                "time": 17258972333,
-                "uid": "42b8c45d-e98d-4f59-9ce8-8f14134c90bd",
-            },
-        },
-    ),
-    # A scan that's not a saved motor position
-    "9bcd07e9-3188-49d3-a1ce-e3b51ebe48b5": MapAdapter(
-        {},
-        metadata={
-            "plan_name": "xafs_scan",
-            "time": 1725897133,
-        },
-    ),
-}
 
 
 @pytest.fixture()
@@ -249,17 +87,6 @@ def test_get_motor_position(client):
     assert result.motors[0].name == "motor_A"
     assert result.motors[0].readback == 12.0
 
-def test_get_motor_position_by_uid_multiple_motors(mongodb):
-    uid = str(mongodb.multiple_motors_positions.find_one({"name": "Good Sample position"})["_id"])
-    print(uid)
-    result = get_motor_position(uid=uid, collection=mongodb.multiple_motors_positions)
-    assert result.name == "Good Sample position"
-    assert result.motors[0].name == "sam_H"
-    assert result.motors[0].readback == 10.1
-    assert result.motors[0].offset == 0.1
-    assert result.motors[1].name == "sam_V"
-    assert result.motors[1].readback == -20.95
-    assert result.motors[1].offset == -0.05
 
 async def test_get_motor_positions(client):
     results = get_motor_positions(after=1725897100, before=1725897200)
