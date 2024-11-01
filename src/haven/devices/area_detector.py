47406f385d41700be071587cf582070ab7ff0239
yannachen <chen.yanna2022@gmail.com>
2024-11-01 16:42:36 -0500

    Updated the threaded ophyd area detectors so they can use the same write path template as ophyd-async.

--- src/haven/devices/area_detector.py
+++ src/haven/devices/area_detector.py
@@ -316,18 +316,25 @@ class DynamicFileStore(Device):
     """File store mixin that alters the write_path_template based on
     iconfig values.
 
+    Parameters
+    ==========
+    write_path_template
+      A format string to use for deciding file paths. Can accept keys
+      {root_path} and {name}.
+
     """
 
     def __init__(
-        self, *args, write_path_template="/{root_path}/%Y/%m/{name}/", **kwargs
+        self, *args, write_path_template="{root_path}/%Y/%m/{name}/", **kwargs
     ):
+        write_path_template = write_path_template.lstrip('/')
         super().__init__(*args, write_path_template=write_path_template, **kwargs)
         # Format the file_write_template with per-device values
         config = load_config()
         try:
             self.write_path_template = self.write_path_template.format(
                 name=self.parent.name,
-                root_path=config.get("area_detector_root_path", "/tmp"),
+                root_path=config.get("area_detector_root_path", "/tmp").lstrip('/'),
             )
         except KeyError:
             warnings.warn(f"Could not format write_path_template {write_path_template}")
