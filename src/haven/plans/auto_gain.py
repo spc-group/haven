2f6e8fba18adbdec0123d3743940266815fd878e
yannachen <chen.yanna2022@gmail.com>
2024-11-14 10:39:46 -0600

    Fixed a bug in calculating gain values in the auto_gain plan.

--- src/haven/plans/auto_gain.py
+++ src/haven/plans/auto_gain.py
@@ -31,7 +31,7 @@ class GainRecommender:
     gain_max: int = 27
     last_point: np.ndarray = None
     dfs: list = None
-    big_step: int = 3
+    big_step: int = 1
 
     def __init__(
         self, volts_min: float = 0.5, volts_max: float = 4.5, target_volts: float = 2.5
@@ -95,7 +95,7 @@ class GainRecommender:
         values_in_range = df[(df.volts < self.volts_max) & (df.volts > self.volts_min)]
         needed_gains = np.arange(
             df[df.volts < self.volts_min].gain.max() + 1,
-            df[df.volts > self.volts_max].gain.max(),
+            df[df.volts > self.volts_max].gain.min(),
         )
         missing_gains = [
             gain for gain in needed_gains if gain not in values_in_range.gain
