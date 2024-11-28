a7b64cb3f676cdb8f7c8b716269484c08056c968
25-ID-D user <s25idduser@lerix.xray.aps.anl.gov>
2024-11-28 14:27:26 -0600

    Updated the xspress3 async detector so that it can properly count in a plan.

--- src/haven/devices/detectors/xspress.py
+++ src/haven/devices/detectors/xspress.py
@@ -1,8 +1,19 @@
 import asyncio
 from collections.abc import Sequence
 
-from ophyd_async.core import PathProvider, SignalR, StandardDetector, DetectorController, DetectorTrigger, AsyncStatus, TriggerInfo, StrictEnum, set_and_wait_for_value
+from ophyd_async.core import (
+    PathProvider,
+    SignalR,
+    StandardDetector,
+    DetectorController,
+    DetectorTrigger,
+    AsyncStatus,
+    TriggerInfo,
+    StrictEnum,
+    set_and_wait_for_value,
+)
 from ophyd_async.epics import adcore
+from ophyd_async.epics.core import epics_signal_rw, epics_signal_x
 
 from .area_detectors import default_path_provider, HavenDetector
 
@@ -18,6 +29,14 @@ class XspressTriggerMode(StrictEnum):
     SOFTWARE_INTERNAL = "Software + Internal"
 
 
+class XspressDriverIO(adcore.ADBaseIO):
+    def __init__(self, prefix, name=""):
+        self.trigger_mode = epics_signal_rw(XspressTriggerMode, f"{prefix}TriggerMode")
+        self.erase_on_start = epics_signal_rw(bool, f"{prefix}EraseOnStart")
+        self.erase = epics_signal_x(f"{prefix}ERASE")
+        super().__init__(prefix=prefix, name=name)
+
+
 class XspressController(DetectorController):
     def __init__(self, driver: adcore.ADBaseIO) -> None:
         self._drv = driver
@@ -26,22 +45,23 @@ class XspressController(DetectorController):
         # Xspress deadtime handling
         return 0.001
 
+    @AsyncStatus.wrap
     async def prepare(self, trigger_info: TriggerInfo):
         await asyncio.gather(
-            self._drv.num_images.set(trigger_info.total_number_of_images),
+            self._drv.num_images.set(trigger_info.total_number_of_triggers),
             self._drv.image_mode.set(adcore.ImageMode.multiple),
             self._drv.trigger_mode.set(XspressTriggerMode.INTERNAL),
         )
-        if exposure is not None:
-            await self._drv.acquire_time.set(exposure)
-        return await adcore.start_acquiring_driver_and_ensure_status(self._drv)
 
     async def wait_for_idle(self):
         if self._arm_status:
             await self._arm_status
 
     async def arm(self):
-        self._arm_status = await set_and_wait_for_value(self._drv.acquire, True)    
+        self._arm_status = await adcore.start_acquiring_driver_and_ensure_status(
+            self._drv
+        )
+
     async def disarm(self):
         await adcore.stop_busy_record(self._drv.acquire, False, timeout=1)
 
@@ -59,7 +79,7 @@ class Xspress3Detector(HavenDetector, StandardDetector):
         name: str = "",
         config_sigs: Sequence[SignalR] = (),
     ):
-        self.drv = adcore.ADBaseIO(prefix + drv_suffix)
+        self.drv = XspressDriverIO(prefix + drv_suffix)
         self.hdf = adcore.NDFileHDFIO(prefix + hdf_suffix)
 
         if path_provider is None:
@@ -74,4 +94,12 @@ class Xspress3Detector(HavenDetector, StandardDetector):
             ),
             config_sigs=(self.drv.acquire_period, self.drv.acquire_time, *config_sigs),
             name=name,
+        )
+
+    @AsyncStatus.wrap
+    async def stage(self) -> None:
+        await asyncio.gather(
+            super().stage(),
+            self.drv.erase_on_start.set(False),
+            self.drv.erase.trigger(),
         )
