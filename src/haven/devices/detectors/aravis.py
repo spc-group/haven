d79b465a16dafd13eadaac5393912db707b3978c
yannachen <chen.yanna2022@gmail.com>
2024-12-12 09:56:09 -0600

    Fixes to the aravis support to try and get bloptools working.

--- src/haven/devices/detectors/aravis.py
+++ src/haven/devices/detectors/aravis.py
@@ -1,10 +1,15 @@
 from ophyd_async.core import SubsetEnum
 from ophyd_async.epics.adaravis import AravisDetector as DetectorBase
-from ophyd_async.epics.signal import epics_signal_rw_rbv
+from ophyd_async.epics.signal import epics_signal_rw_rbv, epics_signal_r
+from numpy.typing import NDArray
+import numpy as np
 
 from .area_detectors import HavenDetector
 
 AravisTriggerSource = SubsetEnum["Software", "Line1"]
+
+
+AravisPixelFormat = SubsetEnum["Mono8"]
 
 
 class AravisDetector(HavenDetector, DetectorBase):
@@ -12,6 +17,11 @@ class AravisDetector(HavenDetector, DetectorBase):
 
     def __init__(self, prefix, *args, **kwargs):
         super().__init__(*args, prefix=prefix, **kwargs)
+        self.image_array = epics_signal_r(NDArray[np.int16], f"{prefix}image1:ArrayData")
+        self.drv.pixel_format = epics_signal_rw_rbv(
+            AravisPixelFormat,
+            f"{prefix}cam1:PixelFormat",
+        )
         # Replace a signal that has different enum options
         self.drv.trigger_source = epics_signal_rw_rbv(
             AravisTriggerSource,  # type: ignore
