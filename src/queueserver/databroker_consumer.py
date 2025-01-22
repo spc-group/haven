6e448064a15c74b952cbf08bbb9822bfe01c4c44
25-ID Staff Account <25-id@list.anl.gov>
2025-01-22 11:02:14 -0600

    Re-wrote the mongo_consumer to use databroker catalogs directly.

--- src/queueserver/databroker_consumer.py
+++ src/queueserver/databroker_consumer.py
@@ -3,18 +3,17 @@ import sys
 from typing import Mapping, Sequence
 
 import msgpack
+import databroker
 from bluesky.callbacks.tiled_writer import TiledWriter
 from bluesky_kafka import BlueskyConsumer
-from tiled.client import from_uri
-from tiled.client.base import BaseClient
 
 import haven
 
 log = logging.getLogger(__name__)
 
 
-class TiledConsumer(BlueskyConsumer):
-    """Send Bluesky documents received from a Kafka broker to Tiled for writing.
+class DatabrokerConsumer(BlueskyConsumer):
+    """Send Bluesky documents received from a Kafka broker to a databroker catalog.
 
     There is no default configuration. A reasonable configuration for production is
         consumer_config={
@@ -23,8 +22,6 @@ class TiledConsumer(BlueskyConsumer):
 
     Parameters
     ----------
-    tiled_client
-      The top-level Tiled client object to use for writing.
     topic_catalog_map
       Translates Kafka topic names to Tiled catalogs. Each value
       should be the name of a catalog available directly under
@@ -47,7 +44,6 @@ class TiledConsumer(BlueskyConsumer):
 
     def __init__(
         self,
-        tiled_client: BaseClient,
         topic_catalog_map: Mapping,
         bootstrap_servers: Sequence[str],
         group_id: str,
@@ -59,7 +55,7 @@ class TiledConsumer(BlueskyConsumer):
         # Create writers for each Tiled catalog
         catalog_names = set(topic_catalog_map.values())
         self.writers = {
-            catalog: TiledWriter(tiled_client[catalog]) for catalog in catalog_names
+            name: databroker.catalog[name].v1.insert for name in catalog_names
         }
         super().__init__(
             topics=list(topic_catalog_map.keys()),
@@ -99,22 +95,14 @@ def main():
     config = haven.load_config()
     bootstrap_servers = ["localhost:9092"]
     topic_catalog_map = {
-        "25idc.bluesky.documents": "haven",
-        "25idd.bluesky.documents": "haven",
-        "25idc-dev.bluesky.documents": "haven-dev",
-        "25idd-dev.bluesky.documents": "haven-dev",
+        "25idc.bluesky.documents": "25idc_direct",
+        "25idd.bluesky.documents": "25idd_direct",
     }
-    # Create a tiled writer that will write documents to tiled
-    tiled_uri = config["tiled"]["uri"]
-    tiled_api_key = config["tiled"]["api_key"]
-    client = from_uri(tiled_uri, api_key=tiled_api_key, include_data_sources=True)
-
     # Create a Tiled consumer that will listen for new documents.
-    consumer = TiledConsumer(
-        tiled_client=client,
+    consumer = DatabrokerConsumer(
         topic_catalog_map=topic_catalog_map,
         bootstrap_servers=bootstrap_servers,
-        group_id="tiled_writer",
+        group_id="databroker_consumer",
         consumer_config={"auto.offset.reset": "latest"},
         polling_duration=1.0,
     )
@@ -124,3 +112,59 @@ def main():
 
 if __name__ == "__main__":
     sys.exit(main())
+
+
+# import os
+# import sys
+# import logging
+# from functools import partial
+
+# # This environmental variable needs to be set before importing haven
+# # os.environ["HAVEN_CONFIG_FILES"] = f"{os.environ['BLUESKY_DIR']}/iconfig.toml"
+
+# import databroker
+# import msgpack
+# import msgpack_numpy as mpn
+# from bluesky_kafka import MongoConsumer
+
+# import haven
+
+
+# log = logging.getLogger()
+
+
+# def main():
+#     """Launch the mongo consumer."""
+#     logging.basicConfig(level=logging.INFO)
+
+#     bootstrap_servers = "localhost:9092"
+#     mongo_uri = haven.load_config()['mongo_consumer']['mongo_uri']
+
+#     kafka_deserializer = partial(msgpack.loads, object_hook=mpn.decode)
+#     auto_offset_reset = "latest"
+
+#     topic_database_map = {
+#         "bluesky.documents.haven": "25idc-bluesky",
+#     }
+#     topics = list(topic_database_map.keys())
+
+#     # Create a MongoConsumer that will automatically listen to new beamline topics.
+#     # The parameter metadata.max.age.ms determines how often the consumer will check for
+#     # new topics. The default value is 5000ms.
+#     mongo_consumer = MongoConsumer(
+#         mongo_uri,
+#         topic_database_map,
+#         tls=False,
+#         topics=topics,
+#         bootstrap_servers=bootstrap_servers,
+#         group_id="mongodb",
+#         consumer_config={"auto.offset.reset": auto_offset_reset},
+#         polling_duration=1.0,
+#         deserializer=kafka_deserializer,
+#     )
+#     log.info("Starting mongo consumer")
+#     mongo_consumer.start()
+
+
+# if __name__ == "__main__":
+#     sys.exit(main())
