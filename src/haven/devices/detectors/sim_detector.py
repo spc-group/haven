1e506816b72a8ca49ad359d4538d3384c8461116
Yanna Chen <chen.yanna2022@gmail.com>
2025-01-29 23:41:24 -0600

    update sim_detector

--- src/haven/devices/detectors/sim_detector.py
+++ src/haven/devices/detectors/sim_detector.py
@@ -1,7 +1,11 @@
 from ophyd_async.epics.adsimdetector import SimDetector as SimDetectorBase
-
+from haven._iconfig import load_config 
 from .area_detectors import HavenDetector
 
 
 class SimDetector(HavenDetector, SimDetectorBase):
     _ophyd_labels_ = {"area_detectors", "detectors"}
+
+    def __init__(self, prefix, path_provider=None, **kwargs):
+        path_provider = path_provider or load_config().get("area_detector_root_path", "/default/path")
+        super().__init__(prefix=prefix, path_provider=path_provider, **kwargs)
