3de9c21540c4168ad05c5a925169f05ae4f0db46
Yanna Chen <chen.yanna2022@gmail.com>
2025-02-11 23:30:50 -0600

    isort mannually

--- src/haven/devices/area_detector.py
+++ src/haven/devices/area_detector.py
@@ -8,19 +8,16 @@ from typing import Dict, Mapping
 import numpy as np
 import pandas as pd
 from apstools.devices import CamMixin_V34, SingleTrigger_V34
-from ophyd import ADComponent as ADCpt
 from ophyd import (
+    ADComponent as ADCpt,
     CamBase,
-)
-from ophyd import Component as Cpt
-from ophyd import DetectorBase as OphydDetectorBase
-from ophyd import (
+    Component as Cpt,
+    DetectorBase as OphydDetectorBase,
     Device,
     EigerDetectorCam,
     EpicsSignal,
     Kind,
     Lambda750kCam,
-    OphydObject,
     Signal,
     SimDetectorCam,
     SingleTrigger,
@@ -41,10 +38,8 @@ from ophyd.areadetector.plugins import (
     PvaPlugin_V34,
     ROIPlugin_V31,
     ROIPlugin_V34,
-)
-from ophyd.areadetector.plugins import StatsPlugin_V31 as OphydStatsPlugin_V31
-from ophyd.areadetector.plugins import StatsPlugin_V34 as OphydStatsPlugin_V34
-from ophyd.areadetector.plugins import (
+    StatsPlugin_V31 as OphydStatsPlugin_V31,
+    StatsPlugin_V34 as OphydStatsPlugin_V34,
     TIFFPlugin_V31,
     TIFFPlugin_V34,
 )
