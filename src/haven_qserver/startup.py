42be2a47e88dcfc26a875efc5f35c4907eebe35b
s25idcuser <s25idcuser@aps.anl.gov>
2025-07-15 10:49:45 -0500

    Removed workaround for name mangling and namedmover bugs that have been fixed upstream.

--- src/haven_qserver/startup.py
+++ src/haven_qserver/startup.py
@@ -3,7 +3,7 @@ import re  # noqa: F401
 
 import databroker  # noqa: F401
 from bluesky.plan_stubs import abs_set  # noqa: F401
-from bluesky.plan_stubs import mv as _mv  # noqa: F401
+from bluesky.plan_stubs import mv as mv  # noqa: F401
 from bluesky.plan_stubs import mvr, null, pause, rel_set, sleep, stop  # noqa: F401
 from bluesky.run_engine import call_in_bluesky_event_loop
 from ophyd_async.core import NotConnected
@@ -41,53 +41,6 @@ try:
     call_in_bluesky_event_loop(beamline.connect())
 except NotConnected as exc:
     log.exception(exc)
-for cpt in beamline.devices.all_devices:
-    # Make sure we're not adding a readback value with the same name
-    # as its parent.
-    if cpt.parent is not None and cpt.name == cpt.parent.name:
-        continue
-    # Replace spaces and other illegal characters in variable name
-    # name = re.sub('\W|^(?=\d)','_', cpt.name)
-    name = sanitize_name(cpt.name)
+for device in beamline.devices.root_devices:
     # Add the device as a variable in module's globals
-    globals().setdefault(name, cpt)
-
-
-# Workaround for https://github.com/bluesky/bluesky-queueserver/issues/310
-from collections.abc import Hashable
-
-
-def mv(
-    *args,
-    group: Hashable | None = None,
-    **kwargs,
-):
-    """
-    Move one or more devices to a setpoint. Wait for all to complete.
-
-    If more than one device is specified, the movements are done in parallel.
-
-    Parameters
-    ----------
-    args :
-        device1, value1, device2, value2, ...
-    group : string, optional
-        Used to mark these as a unit to be waited on.
-    kwargs :
-        passed to obj.set()
-
-    Yields
-    ------
-    msg : Msg
-
-    Returns
-    -------
-    statuses :
-        Tuple of n statuses, one for each move operation
-
-    See Also
-    --------
-    :func:`bluesky.plan_stubs.abs_set`
-    :func:`bluesky.plan_stubs.mvr`
-    """
-    yield from _mv(*args, group=group, **kwargs)
+    globals().setdefault(device.name, device)
