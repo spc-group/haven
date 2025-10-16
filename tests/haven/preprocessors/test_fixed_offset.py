8f43c49da6e78c74f2831cf7db272842869e5249
Mark Wolfman <canismarko@gmail.com>
2025-10-16 16:37:24 -0500

    Added calculated fixed-offset mono tests from Aleks.

--- tests/haven/preprocessors/test_fixed_offset.py
+++ tests/haven/preprocessors/test_fixed_offset.py
@@ -1,5 +1,7 @@
+from pathlib import Path
 from unittest import mock
 
+import pandas as pd
 import pytest
 from bluesky import Msg
 from bluesky.utils import ensure_generator
@@ -8,6 +10,29 @@ from haven.devices import AxilonMonochromator, ChannelCutMonochromator
 from haven.preprocessors import fixed_offset_wrapper
 from haven.preprocessors.fixed_offset import beam_offset
 from haven.units import ureg
+
+# Load calculated data from Aleks
+df_parameters = pd.read_excel(
+    "tests/haven/preprocessors/fixed_offset_calculations.xlsx",
+    nrows=8,
+    usecols=[2, 3, 4],
+    index_col=0,
+    skiprows=1,
+)
+df_parameters = df_parameters.T
+df = pd.read_excel(
+    Path(__file__).parent / "fixed_offset_calculations.xlsx", skiprows=12
+)
+df = df.rename(
+    columns={
+        "Energy eV": "energy",
+        "Energy nm": "wavelength",
+        "Bragg Angle rad": "primary_bragg_angle",
+        "Offset (um)": "primary_beam_offset",
+        "Bragg Angle rad.1": "secondary_bragg_angle",
+        "Offset (um).1": "secondary_beam_offset",
+    }
+)
 
 
 @pytest.fixture()
@@ -41,8 +66,10 @@ def test_beam_offset():
     assert beam_offset(bragg, gap=gap).magnitude == pytest.approx(offset.magnitude)
 
 
-def test_bragg_tracking(primary_mono, secondary_mono):
-    bragg = 67407.927958
+@pytest.mark.parametrize("idx, row", df.iloc[1::10].iterrows())
+def test_bragg_tracking(primary_mono, secondary_mono, idx, row):
+    row0 = df.iloc[0]
+    bragg = row["secondary_bragg_angle"]
     input_plan = ensure_generator(
         [
             Msg("set", secondary_mono.bragg, bragg, group="set-43367a", run=None),
@@ -51,37 +78,38 @@ def test_bragg_tracking(primary_mono, secondary_mono):
     )
     wrapped = fixed_offset_wrapper(input_plan, primary_mono, secondary_mono)
     msg = next(wrapped)  # Prime the generative coroutine
-    primary_beam_offset = 10_000.0
-    secondary_beam_offset = 7503.055409
-    d_spacing = 1.92  # Å
-    gap = 4000.0  # µm
-    wrapped.send(units_tasks({"secondary_mono-bragg": {"units": "arcseconds"}}))
+    primary_beam_offset = row0["primary_beam_offset"]
+    secondary_beam_offset = row0["secondary_beam_offset"]
+    d_spacing = df_parameters["Si(220) D Spacing:"].iloc[0]
+    gap = df_parameters["Si(220) Gap"].iloc[0]
+    bragg_offset = df_parameters["Si(220) Bragg Offset:"].iloc[0]
+    wrapped.send(units_tasks({"secondary_mono-bragg": {"units": "radian"}}))
     wrapped.send(units_tasks({"secondary_mono-energy": {"units": "eV"}}))
     wrapped.send({"readback": primary_beam_offset})
     wrapped.send(units_tasks({"primary_mono-beam_offset": {"units": "microns"}}))
     wrapped.send({secondary_mono.beam_offset.name: {"value": secondary_beam_offset}})
     wrapped.send(units_tasks({"secondary_mono-beam_offset": {"units": "microns"}}))
-    wrapped.send({"readback": -2159.15})  # Bragg offset
+    wrapped.send({"readback": bragg_offset})
     wrapped.send(units_tasks({"secondary_mono-bragg_offset": {"units": "arcsec"}}))
     wrapped.send({"readback": gap})
-    wrapped.send(units_tasks({"secondary_mono-gap": {"units": "um"}}))
+    wrapped.send(units_tasks({"secondary_mono-gap": {"units": "mm"}}))
     wrapped.send({"readback": d_spacing})
-    new_msg = wrapped.send(
-        units_tasks({"secondary_mono-d_spacing": {"units": "angstrom"}})
-    )
-    assert new_msg.args[0] == pytest.approx(9900)
+    new_msg = wrapped.send(units_tasks({"secondary_mono-d_spacing": {"units": "nm"}}))
+    assert new_msg.args[0] == pytest.approx(row["primary_beam_offset"])
     other_msgs = list(wrapped)
     assert len(other_msgs) == 2
 
 
-def test_energy_tracking(primary_mono, secondary_mono):
+@pytest.mark.parametrize("idx, row", df.iloc[1::10].iterrows())
+def test_energy_tracking(primary_mono, secondary_mono, idx, row):
     # E = hc/2/d/sin(θ)
     #
     # F = 12398.42
     # B = 1.92
     #
     # F/2/B/sin((A+D)/R2S)
-    energy = 10057.9
+    row0 = df.iloc[0]
+    energy = row["energy"]
     input_plan = ensure_generator(
         [
             Msg("set", secondary_mono.energy, energy, group="set-43367a", run=None),
@@ -89,25 +117,24 @@ def test_energy_tracking(primary_mono, secondary_mono):
         ]
     )
     wrapped = fixed_offset_wrapper(input_plan, primary_mono, secondary_mono)
-    next(wrapped)
-    primary_beam_offset = 10_000.0
-    secondary_beam_offset = 7503.055409
-    d_spacing = 1.92  # Å
-    gap = 4000.0  # µm
-    wrapped.send(units_tasks({"secondary_mono-bragg": {"units": "arcseconds"}}))
+    msg = next(wrapped)  # Prime the generative coroutine
+    primary_beam_offset = row0["primary_beam_offset"]
+    secondary_beam_offset = row0["secondary_beam_offset"]
+    d_spacing = df_parameters["Si(220) D Spacing:"].iloc[0]
+    gap = df_parameters["Si(220) Gap"].iloc[0]
+    bragg_offset = df_parameters["Si(220) Bragg Offset:"].iloc[0]
+    wrapped.send(units_tasks({"secondary_mono-bragg": {"units": "radian"}}))
     wrapped.send(units_tasks({"secondary_mono-energy": {"units": "eV"}}))
     wrapped.send({"readback": primary_beam_offset})
     wrapped.send(units_tasks({"primary_mono-beam_offset": {"units": "microns"}}))
     wrapped.send({secondary_mono.beam_offset.name: {"value": secondary_beam_offset}})
     wrapped.send(units_tasks({"secondary_mono-beam_offset": {"units": "microns"}}))
-    wrapped.send({"readback": -2159.15})  # Bragg offset
+    wrapped.send({"readback": bragg_offset})
     wrapped.send(units_tasks({"secondary_mono-bragg_offset": {"units": "arcsec"}}))
     wrapped.send({"readback": gap})
-    wrapped.send(units_tasks({"secondary_mono-gap": {"units": "um"}}))
+    wrapped.send(units_tasks({"secondary_mono-gap": {"units": "mm"}}))
     wrapped.send({"readback": d_spacing})
-    new_msg = wrapped.send(
-        units_tasks({"secondary_mono-d_spacing": {"units": "angstrom"}})
-    )
-    assert new_msg.args[0] == pytest.approx(9900)
+    new_msg = wrapped.send(units_tasks({"secondary_mono-d_spacing": {"units": "nm"}}))
+    assert new_msg.args[0] == pytest.approx(row["primary_beam_offset"])
     other_msgs = list(wrapped)
     assert len(other_msgs) == 2
